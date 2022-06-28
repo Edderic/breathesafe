423dcae699069269ab0b6cc2a08ec67ce44bea48
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-06-28 09:14:26 -0400

    [feat] add application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,25 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import VueGoogleMaps from 'vue-google-maps';
+import Vue3Geolocation from 'vue3-geolocation';
+import App from './app';
+import { useEventStore } from './stores/event_store.js';
+import { useMainStore } from './stores/main_store.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = createApp(App);
+  const pinia = createPinia();
+  // A store could be good for Manipulating the Google Maps component via a
+  // search component contained in another.
+
+  app.use(VueGoogleMaps, {
+      load: {
+          key: window.gon.GOOGLE_MAPS_API_KEY,
+          libraries: 'places'
+      },
+  // }).use(pinia).use(Vue3Geolocation).mount('#app')
+  }).use(pinia).use(Vue3Geolocation).mount('#app')
+
+  const mainStore = useMainStore();
+  const eventsStore = useEventStore();
+});
