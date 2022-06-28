979579fc5379340706d43cc110aa9122bf9c49c2
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-06-28 09:21:19 -0400

    [feat] use VuePlugin in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
+import VuePlugin from '@vitejs/plugin-vue'
 import RubyPlugin from 'vite-plugin-ruby'
 
 export default defineConfig({
   plugins: [
+    VuePlugin(),
     RubyPlugin(),
   ],
 })
