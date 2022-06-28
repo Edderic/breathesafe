19bd2d1aac777ca3dd7ea597a88ce41622e535c8
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-06-28 09:21:47 -0400

    [feat] add EventsController#new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,15 @@
 class EventsController < ApplicationController
   def new
+    respond_to do |format|
+      format.html do
+        gon.GOOGLE_MAPS_API_KEY = ENV['GOOGLE_MAPS_API_KEY']
+      end
+
+      format.json do
+        render json: {
+          projects: projects
+        }.to_json
+      end
+    end
   end
 end
