725238425bf5b2616a95755c563f5284d5b9205c
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-06-29 21:53:07 -0400

    [feat] add to the beginning of the array

--- app/javascript/event.vue
+++ app/javascript/event.vue
@@ -235,7 +235,7 @@ export default {
     ...mapActions(useEventStore, ['removeActivityGroup']),
     ...mapState(useEventStore, ['findActivityGroup']),
     addActivityGrouping() {
-      this.activityGroups.push({
+      this.activityGroups.unshift({
         'id': this.generateUUID(),
         'aerosolGenerationActivity': "",
         'carbonDioxideGenerationActivity': "",
