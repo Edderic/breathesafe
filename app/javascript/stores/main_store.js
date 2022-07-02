bcd5146229a0de8aaef5627e4e4671fceaf5cd84
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-02 08:56:15 -0400

    [feat] sign out: main_store

--- app/javascript/stores/main_store.js
+++ app/javascript/stores/main_store.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { defineStore } from 'pinia'
 
 // TODO: use the last location that the user was in, as the default
@@ -6,9 +7,28 @@ export const useMainStore = defineStore('main', {
   state: () => ({
     center: {lat: 51.093048, lng: 6.842120},
     zoom: 7,
-    focusTab: 'events'
+    focusTab: 'events',
+    signedIn: false
   }),
   actions: {
+    async isSignedIn() {
+      let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
+      axios.defaults.headers.common['X-CSRF-Token'] = token
+      axios.defaults.headers.common['Accept'] = 'application/json'
+
+      await axios.get(
+        `/registrations/is_signed_in.json`
+      ).then((response) => {
+        signedIn = response.data.isSignedIn
+
+      }).
+        catch((blah) => {
+         console.log('Fail', blah);
+          // TODO: display error
+        });
+
+      console.log(signedIn)
+    },
     setGMapsPlace(center) {
       this.center = center
       this.zoom = 15;
