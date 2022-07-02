ba07bf2f02a9135952f7f26086bd768d56d8d7a7
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-02 08:55:00 -0400

    [feat] setup controller & routes for signing out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/create'
+  devise_for :users, {
+    registrations: 'registrations',
+    sessions: 'users/sessions'
+  }
+
+
   root to: 'events#new'
+
+  devise_scope :user do
+    get 'registrations/is_signed_in', to: 'registrations#is_signed_in'
+    delete 'users/log_out', to: 'users/sessions#destroy'
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
