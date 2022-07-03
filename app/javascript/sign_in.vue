56e45721e928d2cdceefe0ee8cbd5c6849f33c9c
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-03 17:13:35 -0400

    [feat] use users/log_in so that we can override devise controller

--- app/javascript/sign_in.vue
+++ app/javascript/sign_in.vue
@@ -63,7 +63,7 @@ export default {
       let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
       axios.defaults.headers.common['X-CSRF-Token'] = token
       axios.defaults.headers.common['Accept'] = 'application/json'
-      await axios.post('/users/sign_in', {
+      await axios.post('/users/log_in', {
         user: {
           email: this.email,
           password: this.password
