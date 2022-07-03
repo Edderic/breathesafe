4c280c45b44a72ac8c73e77804bcfc13aeb489c6
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-03 12:59:25 -0400

    [feat] use different route so that we could override devise

--- app/javascript/new_sessions.vue
+++ app/javascript/new_sessions.vue
@@ -59,7 +59,7 @@ export default {
       let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
       axios.defaults.headers.common['X-CSRF-Token'] = token
       axios.defaults.headers.common['Accept'] = 'application/json'
-      axios.post('/users/sign_in', {
+      axios.post('/users/log_in', {
         user: {
           email: this.email,
           password: this.password
