360bab04b8ea5b6f9e167795456ddc07bb01c272
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-06 08:53:13 -0400

    [feat] add CarbonDioxideMonitor model

---
+++ app/models/carbon_dioxide_monitor.rb
@@ -0,0 +1,5 @@
+class CarbonDioxideMonitor < ApplicationRecord
+  has_many :users, through: :user_carbon_dioxide_monitors
+
+  validates :serial, uniqueness: { scope: :model, message: "should be unique in the context of a model" }
+end
