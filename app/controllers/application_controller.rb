fa430049f5c0a509e3c76ef435d782f759fc6e92
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-06 15:50:05 -0400

    [feat] create unauthorized? helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, :only => [:create]
   respond_to :html, :json
+
+  def unauthorized?
+    !current_user
+  end
 end
