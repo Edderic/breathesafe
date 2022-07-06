ace31eeb9755cfc3c8cee35115979986f8081bcb
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-06 09:09:03 -0400

    [refactor] use setupCSRF

--- app/javascript/profile.vue
+++ app/javascript/profile.vue
@@ -228,9 +228,7 @@ export default {
       carbonDioxideMonitor['serial'] = event.target.value;
     },
     async load() {
-      let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
-      axios.defaults.headers.common['X-CSRF-Token'] = token
-      axios.defaults.headers.common['Accept'] = 'application/json'
+      setupCSRF();
 
       await axios.get('/users/get_current_user.json')
         .then(response => {
