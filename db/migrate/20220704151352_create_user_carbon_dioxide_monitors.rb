1227460051ecdfe8d50537c55b99e00496387914
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-06 08:39:49 -0400

    [feat] create user carbon dioxide monitors

---
+++ db/migrate/20220704151352_create_user_carbon_dioxide_monitors.rb
@@ -0,0 +1,11 @@
+class CreateUserCarbonDioxideMonitors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_carbon_dioxide_monitors do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :serial, null: false
+      t.string :model, null: false
+
+      t.timestamps
+    end
+  end
+end
