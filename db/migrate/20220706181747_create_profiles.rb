d428b0d97a4cca49ff3c8de27d26915c8ff06fb1
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-09 15:59:17 -0400

    [feat] create profiles table

---
+++ db/migrate/20220706181747_create_profiles.rb
@@ -0,0 +1,10 @@
+class CreateProfiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :measurement_system, null: false
+
+      t.timestamps
+    end
+  end
+end
