18b52c6290e8462db231d51854674ee55f642be8
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-09 15:59:39 -0400

    [feat] add heatmap

---
+++ app/javascript/colored_cell.vue
@@ -0,0 +1,34 @@
+<template>
+  <td
+    :style="{ backgroundColor: cellColor }"
+  >
+    <slot>
+      {{ value / maxVal }}
+    </slot>
+  </td>
+</template>
+
+<script>
+  import { interpolateColor } from './misc';
+
+  export default {
+    computed: {
+      ratio() {
+        return this.value / this.maxVal;
+      },
+      cellColor() {
+        return interpolateColor(this.colorInterpolationScheme, this.ratio);
+      },
+    },
+    methods: {
+    },
+    props: [
+      'value',
+      'colorInterpolationScheme',
+      'maxVal'
+    ]
+  }
+</script>
+
+<style scoped>
+</style>
