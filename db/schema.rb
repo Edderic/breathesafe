7da8774c700ac7c601eb3b92ac4203401d8334b6
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-13 22:22:47 -0400

    [feat] add variables to store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_021908) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_211657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_021908) do
     t.jsonb "occupancy"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "ventilation_ach"
+    t.float "portable_ach"
+    t.float "room_usable_volume_cubic_meters"
+    t.float "total_ach"
     t.index ["author_id"], name: "index_events_on_author_id"
   end
 
