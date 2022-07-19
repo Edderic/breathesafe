850c48613b6b548aedb26f73631696cccf72e87c
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-19 09:15:17 -0400

    [feat] add prevalence to profile

---
+++ db/migrate/20220718214820_add_prevalence_to_profile.rb
@@ -0,0 +1,7 @@
+class AddPrevalenceToProfile < ActiveRecord::Migration[7.0]
+  def change
+    add_column :profiles, :num_positive_cases_last_seven_days, :integer
+    add_column :profiles, :num_people_population, :integer
+    add_column :profiles, :uncounted_cases_multiplier, :float
+  end
+end
