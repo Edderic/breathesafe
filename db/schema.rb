6c42b3e7af7a87fd183bc3c030ce315515974a27
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-18 22:00:55 -0400

    [feat] save new params on schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_17_025200) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_214820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_17_025200) do
     t.string "measurement_system", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "num_positive_cases_last_seven_days"
+    t.integer "num_people_population"
+    t.float "uncounted_cases_multiplier"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
