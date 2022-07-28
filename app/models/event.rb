90156ce4e5cd0f3137c1ecb8cef04bb8a260650f
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-28 07:17:45 -0400

    [feat] when there is no current_user, hide private

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,15 @@
 class Event < ApplicationRecord
 
   def self.can_be_accessed_by(user)
+    unless user
+      return Event.find_by_sql(
+        <<-SQL
+          select * from events where private = 'public'
+        SQL
+      )
+
+    end
+
     Event.find_by_sql(
       <<-SQL
         select * from events where author_id = #{user.id} or private = 'public'
