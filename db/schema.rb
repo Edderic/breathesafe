f09fa20173d65290a357d5942d683e07180b8d06
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-07-28 07:18:07 -0400

    [feat] add event_display_risk_time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_19_114643) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_25_120405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_19_114643) do
     t.integer "num_people_population"
     t.float "uncounted_cases_multiplier"
     t.string "mask_type"
+    t.string "event_display_risk_time"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
