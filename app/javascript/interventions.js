3d612114ea9980db8f4f017e9ee30a1ad8eb265e
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-10 08:09:35 -0400

    [feat] compute durationToLikelyInfection

--- app/javascript/interventions.js
+++ app/javascript/interventions.js
@@ -175,6 +175,92 @@ export class Intervention {
     this.risk = result
     return this.risk
   }
+
+  durationToLikelyInfection() {
+    if (this.durToLikelyInfection) {
+      return this.durToLikelyInfection
+    }
+
+    let lowerBound = 0
+    let upperBound = 5000000
+    let target = 0.99
+
+    this.durToLikelyInfection = binarySearch(
+      lowerBound,
+      upperBound,
+      {
+        'func': this.computeInfection.bind(this),
+        'args': {}
+      },
+      target,
+      'duration'
+    )
+
+    return this.durToLikelyInfection
+  }
+
+  computeInfection(args) {
+    if (!args.duration) {
+      args.duration = 1 // hour
+    }
+
+    let totalAch = this.event.totalAch
+    if (!totalAch) {
+      debugger
+
+    }
+    let uvAch = 0
+    let ventilationAch = 0
+    let portableAch = 0
+    let riskiestActivityGroup = JSON.parse(JSON.stringify(this.riskiestActivityGroup))
+    // When there are no interventions, default to the maskType of the riskiest potential infector
+    let susceptibleMaskPenentrationFactor = maskToPenetrationFactor[riskiestActivityGroup.maskType]
+
+    for (let intervention of this.interventions) {
+      if (intervention.isMask()) {
+        // susceptible and infector wear the same mask
+        riskiestActivityGroup['maskType'] = intervention.maskType
+        susceptibleMaskPenentrationFactor = maskToPenetrationFactor[intervention.maskType]
+      } else if (intervention.isFiltrationAirCleaner()) {
+        portableAch += intervention.computeACH()
+      } else if (intervention.isUpperUV()) {
+        uvAch += intervention.computeACH()
+      }
+
+      totalAch += intervention.computeACH()
+    }
+
+    if (!this.riskiestPotentialInfector['aerosolGenerationActivity'])  {
+      return 0
+    }
+
+    const occupancy = 1
+    const flowRate = totalAch * this.roomUsableVolumeCubicMeters
+    // TODO: consolidate this information in one place
+    const basicInfectionQuanta = 18.6
+    const variantMultiplier = 3.3
+    const quanta = basicInfectionQuanta * variantMultiplier
+    const susceptibleAgeGroup = '30 to <40'
+
+    const susceptibleInhalationFactor = findWorstCaseInhFactor(
+      this.activityGroups,
+      susceptibleAgeGroup
+    )
+    const probaRandomSampleOfOneIsInfectious = 1.0
+
+    return simplifiedRisk(
+      [riskiestActivityGroup],
+      occupancy,
+      flowRate,
+      quanta,
+      susceptibleMaskPenentrationFactor,
+      susceptibleAgeGroup,
+      probaRandomSampleOfOneIsInfectious,
+      args.duration
+    )
+
+  }
+
   numEventsToInfectionWithCertainty() {
     if (this.numEventsToEnsureInfection) {
       return this.numEventsToEnsureInfection
