eaf39f80a6d8d4fe436136bae1636469e92ed1d6
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-17 17:27:32 -0400

    [feat] show clusters on map

--- app/javascript/map_events.vue
+++ app/javascript/map_events.vue
@@ -8,7 +8,40 @@
             map-type-id="terrain"
             class='map'
         >
-          <GMapCluster>
+          <GMapCluster
+              :styles="[
+                {
+                  textColor: 'black',
+                  url: 'https://raw.githubusercontent.com/googlemaps/v3-utility-library/37c2a570c318122df57b83140f5f54665b9359e5/packages/markerclustererplus/images/m1.png',
+                  height: 52,
+                  width: 53,
+                },
+                {
+                  textColor: 'black',
+                  url: 'https://raw.githubusercontent.com/googlemaps/v3-utility-library/37c2a570c318122df57b83140f5f54665b9359e5/packages/markerclustererplus/images/m2.png',
+                  height: 55,
+                  width: 56,
+                },
+                {
+                  textColor: 'black',
+                  url: 'https://raw.githubusercontent.com/googlemaps/v3-utility-library/37c2a570c318122df57b83140f5f54665b9359e5/packages/markerclustererplus/images/m3.png',
+                  height: 65,
+                  width: 66,
+                },
+                {
+                  textColor: 'black',
+                  url: 'https://raw.githubusercontent.com/googlemaps/v3-utility-library/37c2a570c318122df57b83140f5f54665b9359e5/packages/markerclustererplus/images/m4.png',
+                  height: 77,
+                  width: 78,
+                },
+                {
+                  textColor: 'black',
+                  url: 'https://raw.githubusercontent.com/googlemaps/v3-utility-library/37c2a570c318122df57b83140f5f54665b9359e5/packages/markerclustererplus/images/m5.png',
+                  height: 89,
+                  width: 90,
+                },
+              ]"
+          >
             <GMapMarker
                 :key="index"
                 v-for="(m, index) in markers"
