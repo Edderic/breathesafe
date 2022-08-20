6d3aae62811577703b652692bec6570d8145556a
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-20 14:00:24 -0400

    [refactor] remove unused code

--- app/javascript/misc.js
+++ app/javascript/misc.js
@@ -988,9 +988,9 @@ export function displayCADR(systemOfMeasurement, cubicMetersPerHour) {
 
 
 export function setupCSRF() {
-    let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
-    axios.defaults.headers.common['X-CSRF-Token'] = token
-    axios.defaults.headers.common['Accept'] = 'application/json'
+  let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
+  axios.defaults.headers.common['X-CSRF-Token'] = token
+  axios.defaults.headers.common['Accept'] = 'application/json'
 }
 
 export function generateUUID() {
