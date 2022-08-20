087fff5b38a2fd5b0114f7e3277b9930bd89ee35
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-19 22:22:58 -0400

    [feat] model of CovidState

---
+++ app/models/covid_state.rb
@@ -0,0 +1,9 @@
+class CovidState < ApplicationRecord
+  def self.get_latest_date
+    CovidState.connection.exec_query(
+      <<-SQL
+        select max(date) from covid_states
+      SQL
+    )[0]["max"].to_date
+  end
+end
