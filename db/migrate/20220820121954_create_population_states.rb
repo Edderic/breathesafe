320917e5d67ff4a40c37c41210bad7deab4d52ce
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-20 08:41:59 -0400

    [feat] migration to create population states

---
+++ db/migrate/20220820121954_create_population_states.rb
@@ -0,0 +1,10 @@
+class CreatePopulationStates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :population_states do |t|
+      t.integer :population
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
