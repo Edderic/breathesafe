4f2ba8a93718b66f279357bc91dff5fd0921561e
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-20 08:44:48 -0400

    [feat] update events query to include estimate of COVID prevalence

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,41 @@
 class Event < ApplicationRecord
 
   def self.can_be_accessed_by(user)
-    unless user
-      return Event.find_by_sql(
-        <<-SQL
-          select * from events where private = 'public'
-        SQL
-      )
-
-    end
-
-    Event.find_by_sql(
+    # unless user
+      # return Event.find_by_sql(
+        # <<-SQL
+          # select * from events where private = 'public'
+        # SQL
+      # )
+#
+    # end
+    Event.connection.exec_query(
       <<-SQL
-        select * from events where author_id = #{user.id} or private = 'public'
+        select events_with_state.*, num_cases_last_seven_days, population_states.population, num_cases_last_seven_days::float / population_states.population as naive_prevalence from (
+          select events.*, array_to_string(regexp_matches(place_data->>'formatted_address', '[A-Z]{2}'), ';') as state_short_name
+          from events
+        ) as events_with_state
+
+        left join states on (states.code = events_with_state.state_short_name)
+        left join (
+          select (curr_cum - prev_cum) as num_cases_last_seven_days, state from
+            (
+              select blah.date as curr_date, blah.state, blah.cases_cumulative as curr_cum, bleh.cases_cumulative as prev_cum, bleh.date as seven_days_ago_date from (
+                select date, cases_cumulative, state
+                from covid_states
+                where date = '#{Date.today - 1.day}'
+              ) as blah
+              inner join (
+                select date, to_date('#{Date.today - 1.day}', 'YYYY-MM-DD') as recent_date,  state, cases_cumulative from covid_states where date = '#{Date.today - 8.day}'
+              ) as bleh on (
+                blah.date = bleh.recent_date
+                and blah.state = bleh.state
+              )
+           ) as cases
+        ) as cases_2 on (cases_2.state = states.full_name)
+        left join population_states on (
+          population_states.name = states.full_name
+        )
       SQL
     )
   end
