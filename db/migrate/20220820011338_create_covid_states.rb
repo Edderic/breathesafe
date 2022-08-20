cfb540d6ddb8b0d231c8b47384c128ddb1d029e4
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-19 22:23:15 -0400

    [feat] migration of covid state

---
+++ db/migrate/20220820011338_create_covid_states.rb
@@ -0,0 +1,13 @@
+class CreateCovidStates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :covid_states do |t|
+      t.date :date
+      t.string :state
+      t.integer :fips
+      t.integer :cases_cumulative
+      t.integer :deaths_cumulative
+
+      t.timestamps
+    end
+  end
+end
