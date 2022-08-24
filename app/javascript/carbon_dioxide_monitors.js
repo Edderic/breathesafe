b5549c7dce06dd7915062dce29975ec72b71f643
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-23 20:10:23 -0400

    [feat] add carbon dioxide monitors

---
+++ app/javascript/carbon_dioxide_monitors.js
@@ -0,0 +1,38 @@
+export const CARBON_DIOXIDE_MONITORS = [
+  {
+    'name': 'Aranet 4 Home',
+    'website': 'https://www.co2meter.com/products/aranet4-home-indoor-air-quality-monitor',
+    'estimatedCost': '$250',
+    'imageLink': 'https://cdn.shopify.com/s/files/1/0019/5952/products/aranet4-product-photo-ppm-shopify_large.jpg?v=1647088427'
+  },
+  {
+    'name': 'Temtop M2000',
+    'website': 'https://temtopus.com/products/temtop-m2000-2nd-generation-air-quality-monitor-for-pm2-5-pm10-particles-co2-hcho-temperature-humidity-settable-audio-alarm-data-export-recording-curve-easy-calibration',
+    'estimatedCost': '$175',
+    'imageLink': 'https://cdn.shopify.com/s/files/1/2845/1642/products/temtop-m2000-2nd-generation-air-quality-monitor-for-pm25-pm10-particles-co2-hcho-temperature-humidity-settable-audio-alarm-data-export-recording-curve-easy-cali-436116.jpg?v=1587221028'
+  },
+  {
+    'name': 'INKBIRDPLUS',
+    'website': 'https://www.amazon.com/INKBIRDPLUS-Temperature-Relative-Humidity-Indoor-CO2-Meter-Wine-Cellars/dp/B09MRX4F12/',
+    'estimatedCost': '$70',
+    'imageLink': 'https://m.media-amazon.com/images/I/71QxSbnqVbL._SX522_.jpg'
+  },
+  {
+    'name': 'Awair Element',
+    'website': 'https://uk.getawair.com/products/element',
+    'estimatedCost': '£299 / €329',
+    'imageLink': 'https://assets.website-files.com/60ef5335dd0a65fef9eac68c/60ef5335dd0a650e67eac712_element_product_only.png'
+  },
+  {
+    'name': 'CO2 Click',
+    'website': 'https://www.co2.click/ecommerce/en/Products.html',
+    'estimatedCost': '$199 CAD',
+    'imageLink': 'https://www.co2.click/ecommerce/en/images/IMG_20220109_195338_0.jpg'
+  },
+  {
+    'name': 'Vitalight',
+    'website': 'https://checkout.stripe.com/pay/cs_live_b1VCVeuHN7GXsHKP3fmu4Ie1yGzc8hRLe1s1yytCNodgrfi6Xd9E1buFex#fidkdWxOYHwnPyd1blppbHNgWjA0TkBLcV9GSkk2XTRhamBdMF9GZzZATjZLb2loR3NzblFpRn1JaWJLPHZcf3JsYlU9UmpBUWY1cTdJTklsamZMTm9iVn83NF0wM1R%2FZE9ISnVtQXFcbkA0NTV8V1ZLY303bCcpJ3VpbGtuQH11anZgYUxhJz8nPXJIZ05yPGg0YlEwZkQ9MWJAJyknd2BjYHd3YHdKd2xibGsnPydtcXF1dj8qKmFqa2RxYGhkdm4rZmQqJ3gl',
+    'estimatedCost': '$70 CAD',
+    'imageLink': 'https://m.media-amazon.com/images/I/51fb8qXLa2L._AC_SX679_.jpg'
+  },
+]
