e199a53f43da0c9d37584513abf103d6c1e8a6e6
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-28 07:57:38 -0400

    [feat] clean air delivery rate table

---
+++ app/javascript/clean_air_delivery_rate_table.vue
@@ -0,0 +1,156 @@
+
+<template>
+  <table>
+    <tr>
+      <th class='col centered'>
+        <span>Clean Air Delivery Rate</span>
+        <span class='font-light'>({{this.measurementUnits.airDeliveryRateMeasurementTypeShort}})</span>
+      </th>
+      <th></th>
+      <th class='col centered'>
+        <span>Unoccupied Room Volume</span>
+        <span class='font-light'>({{this.measurementUnits.cubicLengthShort}})</span>
+      </th>
+      <th></th>
+      <th class='col centered'>
+        <span>Total ACH</span>
+        <span class='font-light'>(1 / h)</span>
+      </th>
+      <th v-if="systemOfMeasurement == 'imperial'"></th>
+      <th class='col centered' v-if="systemOfMeasurement == 'imperial'">
+        <span></span>
+        <span class='font-light'>(min / h)</span>
+      </th>
+    </tr>
+    <tr>
+      <td>
+      <ColoredCell
+        :colorScheme="colorInterpolationSchemeRoomVolume"
+        :maxVal=1
+        :value='totalFlowRateRounded'
+        :style='cellCSSMerged'
+      />
+      </td>
+      <td>=</td>
+      <td>
+      <ColoredCell
+        :colorScheme="colorInterpolationSchemeRoomVolume"
+        :maxVal=1
+        :value='roomUsableVolumeRounded'
+        :style='cellCSSMerged'
+      />
+      </td>
+      <td>x</td>
+      <td>
+      <ColoredCell
+        :colorScheme="colorInterpolationSchemeTotalAch"
+        :maxVal=1
+        :value='totalAchRounded'
+        :style='cellCSSMerged'
+      />
+      </td>
+      <td v-if="systemOfMeasurement == 'imperial'">/</td>
+      <td>
+      <ColoredCell
+        v-if="systemOfMeasurement == 'imperial'"
+        :colorScheme="colorInterpolationSchemeTotalAch"
+        :maxVal=1
+        :value='60'
+        class='color-cell'
+        :style='cellCSSMerged'
+        style='background-color: grey'
+      />
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+import ColoredCell from './colored_cell.vue'
+import { convertVolume } from './measurement_units.js';
+import {
+  assignBoundsToColorScheme,
+  colorSchemeFall,
+  colorInterpolationSchemeAch,
+  cutoffsVolume,
+} from './colors.js';
+import {
+  displayCADR,
+  round
+} from './misc.js'
+
+export default {
+  name: 'CleanAirDeliveryRateTable',
+  components: {
+    ColoredCell
+  },
+  computed: {
+    cellCSSMerged() {
+      let def = {
+        'font-weight': 'bold',
+        'color': 'white',
+        'text-shadow': '1px 1px 2px black',
+        'text-align': 'center',
+      }
+
+      // return Object.assign(this.cellCSS, default)
+      return Object.assign(def, this.cellCSS)
+    },
+    colorInterpolationSchemeRoomVolume() {
+      return assignBoundsToColorScheme(colorSchemeFall, cutoffsVolume(this.measurementUnits))
+    },
+    colorInterpolationSchemeTotalAch(){
+      return colorInterpolationSchemeAch
+    },
+    roomUsableVolumeRounded() {
+      return round(this.roomUsableVolume, 1)
+    },
+    roomUsableVolume() {
+      return convertVolume(
+        this.intervention.roomUsableVolumeCubicMeters,
+        'meters',
+        this.measurementUnits.lengthMeasurementType
+      )
+    },
+    totalAchRounded() {
+      return round(this.intervention.computeACH(), 1)
+    },
+    totalFlowRateRounded() {
+      return round(this.totalFlowRate, 1)
+    },
+    totalFlowRate() {
+      return displayCADR(
+        this.systemOfMeasurement,
+        this.intervention.roomUsableVolumeCubicMeters * this.intervention.computeACH()
+      )
+    }
+  },
+  props: {
+    cellCSS: Object,
+    intervention: Object,
+    measurementUnits: Object,
+    systemOfMeasurement: String,
+  }
+}
+</script>
+
+<style>
+  .font-light {
+    font-weight: 400;
+  }
+
+  .col {
+    display: flex;
+    flex-direction: column;
+  }
+
+
+  td {
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+  }
+
+
+</style>
