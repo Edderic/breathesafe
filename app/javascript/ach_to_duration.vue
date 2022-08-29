d30c297d6a76acc4326b16a58aadf52896ccce28
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-08-28 21:06:33 -0400

    [feat] implement AchToDuration

---
+++ app/javascript/ach_to_duration.vue
@@ -0,0 +1,103 @@
+<template>
+
+  <div class='container'>
+    With
+      <ColoredCell
+        :colorScheme="colorInterpolationSchemeTotalAch"
+        :maxVal=1
+        :value='totalAchRounded'
+        :style='cellCSSMerged'
+      />
+      ACH, it takes <ColoredCell
+        :colorScheme="removalScheme"
+        :maxVal=1
+        :value='durationMinutesToRemove(0.99)'
+        :style='cellCSSMerged'
+      /> minutes to remove 99% of the virus from the air, after everyone has left the room.
+  </div>
+
+</template>
+
+<script>
+import ColoredCell from './colored_cell.vue'
+import { convertVolume } from './measurement_units.js';
+import { binSearch } from './interventions.js'
+import {
+  assignBoundsToColorScheme,
+  colorSchemeFall,
+  colorInterpolationSchemeAch,
+  cutoffsVolume,
+  minutesToRemovalScheme
+} from './colors.js';
+import {
+  displayCADR,
+  round
+} from './misc.js'
+
+export default {
+  name: 'AchToDuration',
+  components: {
+    ColoredCell
+  },
+  computed: {
+    cellCSSMerged() {
+      let def = {
+        'font-weight': 'bold',
+        'color': 'white',
+        'text-shadow': '1px 1px 2px black',
+        'text-align': 'center',
+        'display': 'inline-block',
+        'padding': '0.5em'
+      }
+
+      // return Object.assign(this.cellCSS, default)
+      return Object.assign(def, this.cellCSS)
+    },
+    ach() {
+      return this.intervention.computeACH()
+    },
+    colorInterpolationSchemeTotalAch(){
+      return colorInterpolationSchemeAch
+    },
+    removalScheme() {
+      return minutesToRemovalScheme()
+    },
+    totalAchRounded() {
+      return round(this.ach, 1)
+    },
+  },
+  methods: {
+    durationMinutesToRemove(toRemove) {
+      return round(60 * -Math.log(1 - toRemove) / this.ach, 1)
+    }
+  },
+  props: {
+    cellCSS: Object,
+    intervention: Object,
+  }
+}
+</script>
+
+<style scoped>
+  .font-light {
+    font-weight: 400;
+  }
+
+  .col {
+    display: flex;
+    flex-direction: column;
+  }
+
+
+  td {
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+  }
+
+  container {
+    width: 80%;
+
+  }
+</style>
