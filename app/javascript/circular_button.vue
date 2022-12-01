a6e5ec262322d851b1ee0f464f8917a8c83168e6
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-12-01 09:44:18 -0500

    [feat] add circular button

---
+++ app/javascript/circular_button.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 80 80" height='3em' width='3em' class='round'>
+    <circle cx="40" cy="40" r="40" fill="rgb(200, 200, 200)"/>
+
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" class='tilted-header'>{{text}}</text>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'CircularButton',
+  components: {
+  },
+  data() {
+    return {}
+  },
+  props: {
+    text: String
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+  .round text {
+    font: 32px Verdana, Helvetica, Arial, sans-serif;
+  }
+
+  .round {
+    margin-left: 1em;
+    margin-right: 1em;
+  }
+</style>
