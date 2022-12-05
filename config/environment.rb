d02f1684620284c789ec1a1017f7235773e7ca4f
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-12-05 08:50:44 -0500

    [feat] setup SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USER_NAME'], # This is the string literal 'apikey', NOT the ID of your API key
+  :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'breathesafe.xyz',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
