eb2648a66574033759c2e70afed458c25e4574e8
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-12-05 10:01:27 -0500

    [fix] Net::SMTPFatalError (550 The from address does not match a verified Sender Identity. Mail cannot be sent until this error is resolved. Visit https://sendgrid.com/docs/for-developers/sending-email/sender-identity/ to see the Sender Identity requirements

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
   :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
-  :domain => 'breathesafe.xyz',
+  :domain => 'em3074.breathesafe.xyz',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
