130b0d7800e97c009cf0f0dc3803f072e1740185
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-12-06 16:31:35 -0500

    [debug] create mailer
    https://docs.sendgrid.com/for-developers/sending-email/rubyonrails

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,10 @@
+class UserNotifierMailer < ApplicationMailer
+  default :from => 'info@breathesafe.xyz'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
+end
