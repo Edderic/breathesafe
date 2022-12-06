d5cf61c6b23b47254bf91d9f83636dbf432e54c0
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-12-06 16:59:52 -0500

    [debug]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
 
+
   has_one :profile
 
   def carbon_dioxide_monitors
