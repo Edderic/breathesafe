caf5bb8b38d7b0ee91d0f070977c93e3ac2f77a3
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-12-07 08:27:58 -0500

    [feat] switch domain to breathesafe.xyz
    So far just getting this:
    2022-12-07T02:17:22.166333+00:00 app[web.1]: I, [2022-12-07T02:17:22.166230 #4]  INFO -- : [5c5ee619-7024-45ad-a555-5ce0d3a7068e] Started POST "/users" for 146.115.173.126 at 2022-12-07 02:17:22 +0000
    2022-12-07T02:17:22.167781+00:00 app[web.1]: I, [2022-12-07T02:17:22.167729 #4]  INFO -- : [5c5ee619-7024-45ad-a555-5ce0d3a7068e] Processing by Devise::RegistrationsController#create as JSON
    2022-12-07T02:17:22.167838+00:00 app[web.1]: I, [2022-12-07T02:17:22.167809 #4]  INFO -- : [5c5ee619-7024-45ad-a555-5ce0d3a7068e]   Parameters: {"user"=>{"email"=>"edderic+12@gmail.com", "password"=>"[FILTERED]"}, "registration"=>{"user"=>{"email"=>"edderic+12@gmail.com", "password"=>"[FILTERED]"}}}
    2022-12-07T02:17:22.416582+00:00 app[web.1]: I, [2022-12-07T02:17:22.416525 #4]  INFO -- : [5c5ee619-7024-45ad-a555-5ce0d3a7068e] Completed 422 Unprocessable Entity in 249ms (Views: 0.7ms | ActiveRecord: 1.0ms | Allocations: 1214)
    2022-12-07T02:17:22.425679+00:00 heroku[router]: at=info method=POST path="/users" host=www.breathesafe.xyz request_id=5c5ee619-7024-45ad-a555-5ce0d3a7068e fwd="146.115.173.126" dyno=web.1 connect=0ms service=253ms status=422 bytes=567 protocol=https

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,16 @@
 # Load the Rails application.
 require_relative "application"
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
   :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
-  :domain => 'heroku.com',
+  :domain => 'breathesafe.xyz',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+
