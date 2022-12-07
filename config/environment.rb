2e330f4790fd2d276fc69bbf40d05487a5ce7e4f
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-12-06 20:21:59 -0500

    Revert "[debug] see if changing order makes a difference"
    This reverts commit 9af79118322ad87e3b720bfbd6438d23674c421f.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
+# Initialize the Rails application.
+Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
@@ -10,6 +12,3 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :enable_starttls_auto => true
 }
-
-# Initialize the Rails application.
-Rails.application.initialize!
