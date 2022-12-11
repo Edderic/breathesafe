0497d38afa37562f82129e3d470776ab3397ce0c
Edderic Ugaddan <eugaddan@panoramaed.com>
2022-12-11 16:28:50 -0500

    [feat] button component

---
+++ app/javascript/button.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='button'>{{text}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  components: {
+  },
+  data() {
+    return {}
+  },
+  props: {
+    text: String
+  },
+  computed: {
+
+  }, methods: {
+  }
+
+}
+</script>
+
+<style scoped>
+  .button {
+    font-weight: bold;
+    border: 1px solid grey;
+    text-align: center;
+    padding: 1em;
+    font-size: 1.5em;
+    background-color: rgb(219, 21, 0);
+    color: white;
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
+</style>
