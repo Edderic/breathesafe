6141360773965c8fd1db99edd2cf4d6457b05271
Edderic Ugaddan <eugaddan@panoramaed.com>
2023-01-04 18:29:07 -0500

    [feat] create behaviors component

---
+++ app/javascript/behaviors.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class='item-span-wide' id='section-behaviors'>
+    <br id='risk-assessment-summary'>
+    <div class='row centered'>
+      <h2 class='centered'>Behaviors</h2>
+      <CircularButton text='?' @click='show = !show'/>
+    </div>
+  </div>
+
+  <div class='item' id='section-behaviors' v-if='show'>
+    <p>
+      Activities that people partake in can affect the probability of
+      transmission of COVID and other respiratory viruses. Activities
+      where the infector is doing lots of exhalation and vocalization (e.g. loudly
+      talking during heavy exercise) could drastically increase the
+      risk of transmission. Likewise, doing activities where
+      susceptibles are inhaling more air in shorter time increases their risk of
+      getting COVID.
+
+    </p>
+    <p>
+
+    </p>
+    <p>
+      Choosing activities where an infector is quiet and at rest, along
+      with susceptibles being at rest, could decrease the risk of
+      airborne transmission.
+    </p>
+  </div>
+</template>
+
+<script>
+import CircularButton from './circular_button.vue'
+
+export default {
+  name: '',
+  components: {
+    CircularButton,
+  },
+  data() {
+    return { show: false }
+  },
+  props: {
+  },
+  computed: {
+
+  }, methods: {
+  }
+
+}
+</script>
+
+<style scoped>
+  .centered {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
+
+</style>
