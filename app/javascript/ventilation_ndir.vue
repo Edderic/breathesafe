83e79132f4a3b0f14d2b93bf08f1d1b9b8c368a9
Edderic Ugaddan <eugaddan@panoramaed.com>
2023-01-18 10:09:10 -0500

    [feat] add VentilationNDIR

---
+++ app/javascript/ventilation_ndir.vue
@@ -0,0 +1,68 @@
+<template>
+    <DrillDownSection
+
+      title='Non-Infectious Air Delivery Rate'
+      :value='roundOut(cadrLocale, 0)'
+      :text='cadrText'
+      :colorScheme='colorInterpSchemeRoomVol'
+    >
+      <p class='explainer'>This is the amount of estimated non-infectious air that is from mechanical ventilation, open windows, and infiltration.</p>
+    </DrillDownSection>
+</template>
+
+<script>
+import { displayCADR, round } from './misc.js'
+import { co2ColorScheme } from './colors.js'
+import ColoredCell from './colored_cell.vue'
+import DrillDownSection from './drill_down_section.vue'
+import CircularButton from './circular_button.vue'
+
+import {
+  colorInterpolationSchemeRoomVolume,
+} from './colors.js';
+
+export default {
+  name: 'VentilationNDIR',
+  components: {
+    ColoredCell,
+    CircularButton,
+    DrillDownSection
+  },
+  data() {
+    return { show: false }
+  },
+  props: {
+    cadr: Number,
+    systemOfMeasurement: String,
+    measurementUnits: Object
+  },
+  computed: {
+    cadrLocale() {
+      return displayCADR(
+        this.systemOfMeasurement,
+        this.cadr
+      )
+    },
+
+    cadrText() {
+      return `${this.roundOut(this.cadrLocale, 0)} ${this.measurementUnits.airDeliveryRateMeasurementTypeShort}`
+    },
+    carbonDioxideColorScheme() {
+      return co2ColorScheme
+    },
+
+    colorInterpSchemeRoomVol() {
+      return colorInterpolationSchemeRoomVolume(this.measurementUnits)
+    },
+  },
+  methods: {
+    roundOut(someValue, numRound) {
+      return round(someValue, numRound)
+    },
+  }
+
+}
+</script>
+
+<style scoped>
+</style>
