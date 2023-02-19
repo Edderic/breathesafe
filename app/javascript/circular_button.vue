5e2ab5a35ab58a361452bf9d8c4610fa651cc019
Edderic Ugaddan <eugaddan@panoramaed.com>
2023-02-18 19:26:36 -0500

    [feat] conditional showing of slot

--- app/javascript/circular_button.vue
+++ app/javascript/circular_button.vue
@@ -2,6 +2,7 @@
   <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 80 80" height='3em' width='3em' class='round'>
     <circle cx="40" cy="40" r="40" fill="rgb(200, 200, 200)"/>
 
+    <slot v-if='!text'></slot>
     <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" class='tilted-header'>{{text}}</text>
   </svg>
 </template>
