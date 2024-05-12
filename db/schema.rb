c96dc97b44d2d6d949eb0f174fe60c7d3609bc02
Edderic Ugaddan <eugaddan@panoramaed.com>
2024-05-12 10:30:52 -0400

    schema update for adding external api token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_122224) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_11_200807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -129,6 +129,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_122224) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.uuid "external_api_token"
+    t.boolean "can_post_via_external_api", default: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
