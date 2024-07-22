4da35c2361c6cf9d92a746e61796640d9e26f88b
Edderic Ugaddan <edderic@gmail.com>
2024-07-21 23:34:55 -0400

    migration to add/edit a few facial measurement columns

---
+++ db/migrate/20240722030245_change_nose_bridge_to_integer_measurements.rb
@@ -0,0 +1,10 @@
+class ChangeNoseBridgeToIntegerMeasurements < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :facial_measurements, :nose_bridge_breadth
+    remove_column :facial_measurements, :nose_bridge_height
+    add_column :facial_measurements, :nasal_root_breadth, :integer
+    add_column :facial_measurements, :nose_protrusion, :integer
+    add_column :facial_measurements, :nose_bridge_height, :integer
+    add_column :facial_measurements, :lip_width, :integer
+  end
+end
