f7b7a0206fd9c65fc059d6d53db6acf020c5860d
Edderic Ugaddan <edderic@gmail.com>
2024-08-01 19:46:23 -0400

    use caching

--- python/mask-recommender/armbrust_scraper.py
+++ python/mask-recommender/armbrust_scraper.py
@@ -1,9 +1,43 @@
 import argparse
 import requests
+from selenium.common.exceptions import WebDriverException
 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import pandas as pd
+from selenium import webdriver
+import time
+
+def retry(lam, browser, options):
+    try:
+        return lam(browser)
+    except WebDriverException as e:
+        browser = webdriver.Chrome(options=options)
+        return lam(browser)
+
+def find_new_table_rows(table_rows):
+    new_table_rows = []
+
+    for table_row in table_rows:
+        attributes = table_row.attrs
+        if attributes['data-type'] in ['Surgical', 'Cloth']:
+            continue
+
+        pfe = float(table_row.findChild('div', 'pfe').attrs['value'])
+        if pfe < 95:
+            continue
+
+        brand = table_row.findChild('a', 'brand')
+        mask_identifier = brand.text.strip()
+
+        # skip if we've already visited this row successfully
+        if data[data['unique_internal_model_code'] == mask_identifier].shape[0] > 0:
+            continue
+
+        new_table_rows.append(table_row)
+
+    return new_table_rows
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -20,43 +54,87 @@ if __name__ == '__main__':
     data_path = args.data_path
     path = f"{data_path}/mask-recommender/Comprehensive Mask Testing – Armbrust American.html"
 
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    browser = webdriver.Chrome(options=options)
+    pathname = f'{data_path}/armbrust-testing-dataset-2024-07-30.csv'
+
+    # TODO: write code to get the index of the last item in the
+    data = pd.read_csv(pathname)
+    data['unique_internal_model_code'] = data['unique_internal_model_code'].str.strip()
     with open(path) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
         table_rows = soup.find_all('div', 'table_row')
 
-        collection = []
+        collection = data
 
-        for table_row in tqdm(table_rows):
+        new_table_rows = find_new_table_rows(table_rows)
+
+        for table_row in tqdm(new_table_rows):
+
+            brand = table_row.findChild('a', 'brand')
+            url = brand.attrs['href']
+
+            retry(lambda browser: browser.get(url), browser, options)
+            # browser.get(url)
+            time.sleep(10)
+            html = retry(lambda browser: browser.page_source, browser, options)
+            mask_page = BeautifulSoup(html, "html.parser")
+            where_to_buy_urls = mask_page.find_all('p', id='mask-source')
+
+            color = ''
+            colors_html = mask_page.find_all('p', id='mask-color-text')
+            if colors_html:
+                color = colors_html[0].text
+
+            where_to_buy_url_1 = ''
+            if where_to_buy_urls and where_to_buy_urls[0].findChild('a'):
+                try:
+                    where_to_buy_url_1 = where_to_buy_urls[0].findChild('a').attrs['href']
+                except Exception as e:
+                    import pdb; pdb.set_trace()
+
+            brand = table_row.findChild('a', 'brand')
+            mask_identifier = brand.text.strip()
+
+            mask_image_url = ''
+            images_html = mask_page.find_all('img')
+            if len(images_html) > 1:
+                mask_image_url = images_html[1].attrs['src']
+
             attributes = table_row.attrs
-            child = requests.get(table_row.findChild('a', 'brand').attrs['href'])
-            mask_page = BeautifulSoup(child.content, 'html.parser')
-            mask_identifiers = mask_page.find_all('div', 'ProductMeta')
-            mask_image_url = mask_page.find_all('img')[1].attrs['src']
-
             style = attributes['data-style']
 
-            mask_identifier = ''
-            if mask_identifiers:
-                mask_identifier = mask_identifiers[0].text.strip()
+            pfe = float(table_row.findChild('div', 'pfe').attrs['value'])
 
-            test_source = {
-                'unique_internal_model_code': mask_identifier,
-                'filter_type': attributes['data-type'],
-                'filtration_efficiency_percent': table_row.findChild('div', 'pfe').attrs['value'],
-                'breathability_pascals': table_row.findChild('div', 'breathability').text,
-                'image_url': mask_image_url,
-                'style': style,
-                'origin': attributes['data-origin'],
-                'manufacturer': attributes['data-manufacturer'],
-                'strap_type': attributes['data-strap'],
-                'test_source': attributes['data-test-source'],
-                'where_to_buy_url': ""
-            }
+            row_data = pd.DataFrame([
+                {
+                    'unique_internal_model_code': mask_identifier,
+                    'color': color,
+                    'filter_type': attributes['data-type'],
+                    'filtration_efficiency_percent': pfe,
+                    'breathability_pascals': table_row.findChild('div', 'breathability').text,
+                    'image_url': mask_image_url,
+                    'style': style,
+                    'origin': attributes['data-origin'],
+                    'manufacturer': attributes['data-manufacturer'],
+                    'strap_type': attributes['data-strap'],
+                    'test_source': attributes['data-test-source'],
+                    'where_to_buy_url': where_to_buy_url_1
+                }
+            ])
 
-            collection.append(test_source)
+            collection = pd.concat([
+                collection,
+                row_data
+            ])
 
-        pathname = f'{data_path}/armbrust-testing-dataset-2024-07-30.csv'
-        print(f"Saving dataset to {pathname}...")
+            print(f"Saving dataset to {pathname}...")
 
-        pd.DataFrame(collection).to_csv(pathname, index=False)
-        print(f"Saved dataset to {pathname}...")
+            pd.DataFrame(collection).to_csv(pathname, index=False)
+            print(f"Saved dataset to {pathname}...")
+
+        browser.close()
+        browser.quit()
