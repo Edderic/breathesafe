874292d8973623f0e33e9adcefbd76b339d443f1
Edderic Ugaddan <edderic@gmail.com>
2024-08-14 14:42:08 -0400

    add osha exercises description

--- app/javascript/fit_test.vue
+++ app/javascript/fit_test.vue
@@ -354,12 +354,190 @@ export default {
           text: "QNFT"
         },
         {
-          text: "Fit Testing"
+          text: "Comfort",
+        },
+      ],
+      oshaExercises: {
+        'Normal breathing': {
+          'description': 'In a normal standing position, without talking, the subject shall breathe normally.'
+        },
+        'Deep breathing': {
+          'description': 'In a normal standing position, the subject shall breathe slowly and deeply, taking caution so as not to hyperventilate.'
+        },
+        'Turning head side to side': {
+          'description': 'Standing in place, the subject shall slowly turn his/her head from side to side between the extreme positions on each side. The head shall be held at each extreme momentarily so the subject can inhale at each side.'
+        },
+
+        'Moving head up and down': {
+          'description': 'Standing in place, the subject shall slowly move his/her head up and down. The subject shall be instructed to inhale in the up position (i.e., when looking toward the ceiling).'
+        },
+        'Talking': {
+          'description': 'The subject shall talk out loud slowly and loud enough so as to be heard clearly by the test conductor. The subject can read from a prepared text such as the Rainbow Passage, count backward from 100, or recite a memorized poem or song.  Rainbow Passage: When the sunlight strikes raindrops in the air, they act like a prism and form a rainbow. The rainbow is a division of white light into many beautiful colors. These take the shape of a long round arch, with its path high above, and its two ends apparently beyond the horizon. There is, according to legend, a boiling pot of gold at one end. People look, but no one ever finds it. When a man looks for something beyond reach, his friends say he is looking for the pot of gold at the end of the rainbow.'
+        },
+        'Grimace': {
+          'description': 'The test subject shall grimace by smiling or frowning. (This applies only to QNFT testing; it is not performed for QLFT)',
+        },
+        'Bending over': {
+          'description': 'The test subject shall bend at the waist as if he/she were to touch his/her toes. Jogging in place shall be substituted for this exercise in those test environments such as shroud type QNFT or QLFT units that do not permit bending over at the waist.'
         }
+      },
+      saccharinInstructions: [
+'The entire screening and testing procedure shall be explained to the test subject prior to the conduct of the screening test.',
+
+'(a) Taste threshold screening. The saccharin taste threshold screening, performed without wearing a respirator, is intended to determine whether the individual being tested can detect the taste of saccharin.',
+
+'(1) During threshold screening as well as during fit testing, subjects shall wear an enclosure about the head and shoulders that is approximately 12 inches in diameter by 14 inches tall with at least the front portion clear and that allows free movements of the head when a respirator is worn. An enclosure substantially similar to the 3M hood assembly, parts # FT 14 and # FT 15 combined, is adequate.',
+
+"(2) The test enclosure shall have a 3⁄4 -inch (1.9 cm) hole in front of the test subject's nose and mouth area to accommodate the nebulizer nozzle.",
+
+'(3) The test subject shall don the test enclosure. Throughout the threshold screening test, the test subject shall breathe through his/her slightly open mouth with tongue extended. The subject is instructed to report when he/she detects a sweet taste.',
+
+'(4) Using a DeVilbiss Model 40 Inhalation Medication Nebulizer or equivalent, the test conductor shall spray the threshold check solution into the enclosure. The nozzle is directed away from the nose and mouth of the person. This nebulizer shall be clearly marked to distinguish it from the fit test solution nebulizer.',
+
+'(5) The threshold check solution is prepared by dissolving 0.83 gram of sodium saccharin USP in 100 ml of warm water. It can be prepared by putting 1 ml of the fit test solution (see (b)(5) below) in 100 ml of distilled water.',
+
+'(6) To produce the aerosol, the nebulizer bulb is firmly squeezed so that it collapses completely, then released and allowed to fully expand.',
+
+'(7) Ten squeezes are repeated rapidly and then the test subject is asked whether the saccharin can be tasted. If the test subject reports tasting the sweet taste during the ten squeezes, the screening test is completed. The taste threshold is noted as ten regardless of the number of squeezes actually completed.',
+
+'(8) If the first response is negative, ten more squeezes are repeated rapidly and the test subject is again asked whether the saccharin is tasted. If the test subject reports tasting the sweet taste during the second ten squeezes, the screening test is completed. The taste threshold is noted as twenty regardless of the number of squeezes actually completed.',
+
+'(9) If the second response is negative, ten more squeezes are repeated rapidly and the test subject is again asked whether the saccharin is tasted. If the test subject reports tasting the sweet taste during the third set of ten squeezes, the screening test is completed. The taste threshold is noted as thirty regardless of the number of squeezes actually completed.',
+
+'(10) The test conductor will take note of the number of squeezes required to solicit a taste response.',
+
+'(11) If the saccharin is not tasted after 30 squeezes (step 10), the test subject is unable to taste saccharin and may not perform the saccharin fit test.',
+
+'Note to paragraph 3(a): If the test subject eats or drinks something sweet before the screening test, he/she may be unable to taste the weak saccharin solution.',
+
+'(12) If a taste response is elicited, the test subject shall be asked to take note of the taste for reference in the fit test.',
+
+'(13) Correct use of the nebulizer means that approximately 1 ml of liquid is used at a time in the nebulizer body.',
+
+'(14) The nebulizer shall be thoroughly rinsed in water, shaken dry, and refilled at least each morning and afternoon or at least every four hours.',
+
+'(b) Saccharin solution aerosol fit test procedure.',
+
+'(1) The test subject may not eat, drink (except plain water), smoke, or chew gum for 15 minutes before the test.',
+
+'(2) The fit test uses the same enclosure described in 3. (a) above.',
+
+'(3) The test subject shall don the enclosure while wearing the respirator selected in section I. A. of this appendix. The respirator shall be properly adjusted and equipped with a particulate filter(s).',
+
+'(4) A second DeVilbiss Model 40 Inhalation Medication Nebulizer or equivalent is used to spray the fit test solution into the enclosure. This nebulizer shall be clearly marked to distinguish it from the screening test solution nebulizer.',
+
+'(5) The fit test solution is prepared by adding 83 grams of sodium saccharin to 100 ml of warm water.',
+
+'(6) As before, the test subject shall breathe through the slightly open mouth with tongue extended, and report if he/she tastes the sweet taste of saccharin.',
+
+'(7) The nebulizer is inserted into the hole in the front of the enclosure and an initial concentration of saccharin fit test solution is sprayed into the enclosure using the same number of squeezes (either 10, 20 or 30 squeezes) based on the number of squeezes required to elicit a taste response as noted during the screening test. A minimum of 10 squeezes is required.',
+
+'(8) After generating the aerosol, the test subject shall be instructed to perform the exercises in section I. A. 14. of this appendix.',
+
+'(9) Every 30 seconds the aerosol concentration shall be replenished using one half the original number of squeezes used initially (e.g., 5, 10 or 15).',
+
+'(10) The test subject shall indicate to the test conductor if at any time during the fit test the taste of saccharin is detected. If the test subject does not report tasting the saccharin, the test is passed.',
+
+'(11) If the taste of saccharin is detected, the fit is deemed unsatisfactory and the test is failed. A different respirator shall be tried and the entire test procedure is repeated (taste threshold screening and fit testing).',
+
+'(12) Since the nebulizer has a tendency to clog during use, the test operator must make periodic checks of the nebulizer to ensure that it is not clogged. If clogging is found at the end of the test session, the test is invalid.',
       ],
       errorMessages: [],
       masks: [],
-      selectedMask: {},
+      results: {
+        'qualitative': {
+          procedure: null,
+          aerosol: {
+            type: 'Not applicable',
+            notes: ''
+          },
+          exercises: [
+            {
+              name: 'Normal breathing',
+              result: null
+            },
+            {
+              name: 'Deep breathing',
+              result: null
+            },
+            {
+              name: 'Turning head side to side',
+              result: null
+            },
+            {
+              name: 'Moving head up and down',
+              result: null
+            },
+            {
+              name: 'Talking',
+              result: null
+            },
+            {
+              name: 'Rainbow passage',
+              result: null
+            },
+            {
+              name: 'Bending over',
+              result: null
+            },
+            {
+              name: 'Normal breathing',
+              result: null
+            }
+          ]
+        },
+        'quantitative': {
+          procedure: 'Full OSHA',
+          aerosol: {
+            type: 'Ambient',
+            initial_count: 0,
+            notes: ''
+          },
+          exercises: [
+            {
+              name: 'Normal breathing',
+              fit_factor: null
+            },
+            {
+              name: 'Deep breathing',
+              fit_factor: null
+            },
+            {
+              name: 'Turning head side to side',
+              fit_factor: null
+            },
+            {
+              name: 'Moving head up and down',
+              fit_factor: null
+            },
+            {
+              name: 'Talking',
+              fit_factor: null
+            },
+            {
+              name: 'Rainbow passage',
+              fit_factor: null
+            },
+            {
+              name: 'Grimace',
+              fit_factor: null
+            },
+            {
+              name: 'Bending over',
+              fit_factor: null
+            },
+            {
+              name: 'Normal breathing',
+              fit_factor: null
+            }
+          ]
+        }
+      },
+      selectedMask: {
+        id: 0,
+        uniqueInternalModelCode: '',
+        hasExhalationValve: false
+      },
       comfort: {
         "How comfortable is the position of the mask on the nose?": null,
         "Is there adequate room for eye protection?": null,
