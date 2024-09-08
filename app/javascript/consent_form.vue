9738d4e8b8d30bdad273ef728f73a4add38472b3
Edderic Ugaddan <edderic@gmail.com>
2024-09-07 22:54:05 -0400

    added section about who can participate

--- app/javascript/consent_form.vue
+++ app/javascript/consent_form.vue
@@ -20,12 +20,16 @@
         Why is this study being done?
       </router-link>
 
-      <router-link :to="tocPath('how-long-study')">
-        How long will you be in this study?
+      <router-link :to="tocPath('who-can-participate')">
+        Who can participate?
       </router-link>
 
       <router-link :to="tocPath('study-procedures')">
         What are the study procedures?
+      </router-link>
+
+      <router-link :to="tocPath('how-long-study')">
+        How long will you be in this study?
       </router-link>
 
       <router-link :to="tocPath('leave-study')">
@@ -123,6 +127,103 @@
 
         </p>
 
+        <div id='who-can-participate'>
+          <br>
+          <br>
+          <h4>Who can participate?</h4>
+        </div>
+
+        <p>
+        As a participant, you will need access to the following:
+        </p>
+
+        <div class='grid-images'>
+          <figure>
+            <a href="https://www.amazon.com/gp/product/B00JALAIIE/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1" target='_blank'>
+              <img class='left-pane-image' src="https://c.media-amazon.com/images/I/6194IWMjJEL._SX522_.jpg" alt='iGaging 6" Digital External Outside Caliper OD for Woodworking'>
+            </a>
+            <figcaption>Digital Caliper</figcaption>
+          </figure>
+
+          <figure>
+            <a href="https://www.amazon.com/dp/B0BGHCTL45?ref=ppx_yo2ov_dt_b_fed_asin_title" target='_blank'>
+              <img class='left-pane-image' src="https://c.media-amazon.com/images/I/71Cwjwnqc6L._SL1500_.jpg" alt='Tape Measure, iBayam Soft Ruler Measuring Tape for Body Weight Loss Fabric Sewing Tailor Cloth Vinyl Measurement Craft Supplies, 60-Inch Double Scale Ruler, 2-Pack White, Blue'>
+            </a>
+            <figcaption>Tape Measure</figcaption>
+          </figure>
+
+          <figure>
+            <a href="https://aiden.health/products/makrite-801" target='_blank'>
+              <img class='left-pane-image' src="https://aiden.health/cdn/shop/files/766B9B3B-9325-407B-B94E-E4C41C6207BB_710x589@2x.png?v=1718600885" alt='Makrite 801-N95 (adj. head straps), NIOSH 84A-9223, 160mmHg fluid resistance'>
+            </a>
+            <figcaption>A Mask</figcaption>
+          </figure>
+        </div>
+
+        <p>
+        You can contribute valuable data by submitting <a
+          href="https://www.cdc.gov/niosh/docs/2018-130/pdfs/2018-130.pdf"
+          target='_blank'>user seal check</a> information. If you know that a
+        mask does not fit your face and has obvious leaks, you could contribute
+        this data in the User Seal Check section of "Adding a Fit Test".
+        </p>
+
+        The following are <strong>optional</strong>:
+
+        <div class='grid-images'>
+          <figure>
+            <a href="https://tsi.com/discontinued-products/portacount%C2%AE-respirator-fit-tester-8038/" target='_blank'>
+              <img class='left-pane-image' src="https://tsi.com/getmetafile/a296129e-5e8d-48d6-a3a7-05a2d5195987/8038_01" alt='PortaCount'>
+            </a>
+            <figcaption>A Quantitative Fit Testing device such as a PortaCount</figcaption>
+          </figure>
+
+          <figure>
+            <a href="https://www.youtube.com/watch?v=Syj_zeNtLGI" target='_blank'>
+              <img class='left-pane-image' src="https://profittesting.com/cdn/shop/articles/MelFitTest4blog2.jpg?v=1634512473" alt='Tape Measure, iBayam Soft Ruler Measuring Tape for Body Weight Loss Fabric Sewing Tailor Cloth Vinyl Measurement Craft Supplies, 60-Inch Double Scale Ruler, 2-Pack White, Blue'>
+            </a>
+            <figcaption>Qualitative Fit Testing equipment</figcaption>
+          </figure>
+
+          <figure>
+            <a href="https://www.verizon.com/smartphones/apple-iphone-14/" target='_blank'>
+              <img class='left-pane-image' src="https://ss7.vzw.com/is/image/VerizonWireless/iphone-14-midnight-fall22-a?wid=930&hei=930&fmt=webp" alt='An iPhone'>
+            </a>
+            <figcaption>A device equipped with a camera</figcaption>
+          </figure>
+        </div>
+
+        <div id='study-procedures'>
+          <br>
+          <br>
+          <h4>What are the study procedures?</h4>
+        </div>
+
+
+        <h5>Phase 1: manual measurements of facial features (using caliper/tape) </h5>
+
+        <p>
+        You will be asked to give information about demographics and facial measurements <router-link :to='{name: "RespiratorUsers"}'>here</router-link>.
+        Once that is filled out, you can add the following fit testing data <router-link :to='{name: "NewFitTest"}'> here</router-link>:
+        </p>
+
+        <ul>
+          <li>user seal check</li>
+          <li>qualitative fit testing (if applicable)</li>
+          <li>quantitative fit testing (if applicable)</li>
+          <li>comfort</li>
+        </ul>
+
+        <p>
+        You will be guided by the UI. Clicking on the "Save and Continue" button will validate the information being saved and will show helpful error messages and general messages. For example, if a user fails the user seal check section, they don't need to add qualitative or quantitative fit testing data (as it is assumed that they will most likely fail either of those more stringent tests).
+        </p>
+
+        <h5>Phase 2: automated inference of facial measurements via selfies </h5>
+
+        <p>Getting facial measurements via caliper and tape can be a bit tedious and could deter adoption of said recommender system. Breathesafe LLC will attempt to simplify this process by inferring facial measurements via selfies. You will be given the opportunity to share selfies to this project to assess and improve the performance of a facial measurement system based on selfies. Given your facial measurements that were taken from phase 1 using caliper and tape, we will assess and improve the accuracy of said inference system.
+        </p>
+
+
         <div id='how-long-study'>
           <br>
           <br>
@@ -180,35 +281,6 @@
             </tr>
           </tbody>
         </table>
-        <div id='study-procedures'>
-          <br>
-          <br>
-          <h4>What are the study procedures?</h4>
-        </div>
-
-
-        <h5>Phase 1: manual measurements of facial features (using caliper/tape) </h5>
-
-        <p>
-        You will be asked to give information about demographics and facial measurements <router-link :to='{name: "RespiratorUsers"}'>here</router-link>.
-        Once that is filled out, you can add the following fit testing data <router-link :to='{name: "NewFitTest"}'> here</router-link>:
-        </p>
-
-        <ul>
-          <li>user seal check</li>
-          <li>qualitative fit testing</li>
-          <li>quantitative fit testing</li>
-          <li>comfort</li>
-        </ul>
-
-        <p>
-        You will be guided by the UI. Clicking on the "Save and Continue" button will validate the information being saved and will show helpful error messages and general messages. For example, if a user fails the user seal check section, they don't need to add qualitative or quantitative fit testing data (as it is assumed that they will most likely fail either of those more stringent tests).
-        </p>
-
-        <h5>Phase 2: automated inference of facial measurements via selfies </h5>
-
-        <p>Getting facial measurements via caliper and tape can be a bit tedious and could deter adoption of said recommender system. Breathesafe LLC will attempt to simplify this process by inferring facial measurements via selfies. You will be given the opportunity to share selfies to this project to assess and improve the performance of a facial measurement system based on selfies. Given your facial measurements that were taken from phase 1 using caliper and tape, we will assess and improve the accuracy of said inference system.
-        </p>
 
         <div id='leave-study'>
           <br>
@@ -384,6 +456,12 @@ export default {
     justify-content: center;
   }
 
+  .grid-images {
+    display: grid;
+    grid-template-columns: 33% 33% 33%;
+    grid-template-rows: auto;
+  }
+
   .main {
     margin-left: 35vw;
     width: 60vw;
@@ -400,13 +478,26 @@ export default {
     top: 4em;
   }
 
+  .left-pane-image {
+    max-width: 15em;
+  }
+
   @media(min-width: 750px) {
     .toc-button {
       display: none;
     }
 
   }
+
+  @media(max-width: 1000px) {
+    .left-pane-image {
+      max-width: 10em;
+    }
+  }
   @media(max-width: 750px) {
+    .left-pane-image {
+      max-width: 8em;
+    }
     .grid {
       display: flex;
       flex-direction: column;
@@ -433,4 +524,5 @@ export default {
   th, td {
     padding: 0.5em;
   }
+
 </style>
