9a85489c43d11c21f3c4a3835cf816df480bde01
Edderic Ugaddan <edderic@gmail.com>
2024-09-30 23:14:23 -0400

    payable datetimes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_11_000441) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_18_154725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -140,6 +140,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_11_000441) do
     t.boolean "has_exhalation_valve"
     t.bigint "author_id", null: false
     t.float "perimeter_mm"
+    t.json "payable_datetimes", default: []
     t.index ["author_id"], name: "index_masks_on_author_id"
   end
 
