f34209f33c9312a73cf0a27a9213947a010d24fd
Edderic Ugaddan <edderic@gmail.com>
2024-12-19 08:09:04 -0500

    SolutionActor create_default_fit_test_solution create_default_sensitivity_solution

--- app/services/solution_actor.rb
+++ app/services/solution_actor.rb
@@ -1,10 +1,125 @@
+#
+# 1 gallon = 3785.41 mL
+# 100 mL
+#
+# Cost of distilled water: $1.50 / 1 gallon
+#
+# $1.50 / gallon * 1 gallon / 3785.41 mL * 100 mL = $0.039
+# So the cost of water to generate sensitivity solution is about 4 cents
+#
 class SolutionActor
+  def self.create_default_sensitivity_solution(
+    uuid:,
+    datetime: nil
+  )
+    if datetime.nil?
+      datetime = DateTime.now
+    end
+
+    self.create_default_solution(
+        uuid: uuid,
+        concentration_type: 'sensitivity',
+        cost: {
+          'material_cost': 0.04,
+          'time_cost': {
+                'amount': 1,
+                'measurement_unit': 'minute'
+           }
+        },
+        datetime: datetime
+    )
+  end
+
+  def self.create_default_fit_test_solution(
+    uuid:,
+    datetime: nil
+  )
+    if datetime.nil?
+      datetime = DateTime.now
+    end
+
+    self.create_default_solution(
+        uuid: uuid,
+        concentration_type: 'fit test',
+        cost: {
+          'material_cost': 4,
+          'time_cost': {
+                'amount': 2,
+                'measurement_unit': 'minutes'
+           }
+        },
+        datetime: datetime
+    )
+  end
+
+  def self.create_default_solution(
+    uuid:,
+    concentration_type:,
+    cost:,
+    datetime: nil
+  )
+    if datetime.nil?
+      datetime = DateTime.now
+    end
+
+    self.create(
+        uuid: uuid,
+        concentration_type: concentration_type,
+        volume_level_proportion: 1,
+        volume: {
+            amount: 100,
+            measurement_units: 'mL'
+        },
+        how: {
+            method: 'DIY',
+            url: 'https://ph.health.mil/PHC%20Resource%20Library/cv19-do-it-yourself-respiratory-fit-testing.pdf'
+        },
+        cost: cost,
+        flavor_type: 'saccharin',
+        datetime: datetime
+    )
+  end
+
+  def self.create_allegro_solution(
+    uuid:,
+    datetime: nil
+  )
+    if datetime.nil?
+      datetime = DateTime.now
+    end
+
+    self.create(
+        uuid: uuid,
+        concentration_type: 'fit test',
+        volume_level_proportion: 1,
+        volume: {
+            amount: 100,
+            measurement_units: 'mL'
+        },
+        how: {
+            method: 'DIY',
+            url: 'https://ph.health.mil/PHC%20Resource%20Library/cv19-do-it-yourself-respiratory-fit-testing.pdf'
+        },
+        cost: {
+          'material_cost': 4,
+          'time_cost': {
+                'amount': 2,
+                'measurement_unit': 'minutes'
+           }
+        },
+        flavor_type: 'saccharin',
+        datetime: datetime
+    )
+  end
+
   def self.create(
+    uuid: ,
     concentration_type:,
     volume:,
     how:,
     flavor_type:,
     cost:,
+    volume_level_proportion:nil,
     datetime: nil
   )
     # Parameters:
@@ -31,22 +146,26 @@ class SolutionActor
       name: 'CreateSolution',
       datetime: datetime,
       metadata: {
-        'uuid' => SecureRandom.uuid,
+        'uuid' => uuid,
         'flavor_type' => flavor_type,
         'concentration_type' => concentration_type,
         'volume' => volume,
+        'volume_level_proportion' => volume_level_proportion,
         'how' => how,
         'cost' => cost
       }
     )
   end
 
-  def self.set_solution_volume(volume:, uuid:, cost:nil, datetime: nil)
+  def self.estimate_volume(volume_level_proportion:, uuid:, datetime: nil)
     # Parameters:
-    #   volume: {
+    #   volume_level_proportion: {
     #     amount: 1,
     #     measurement_unit: 'mL'
     #   },
+    #   uuid: uuid
+    #   datetime: Defaults to nil
+    #     if nil, gets DateTime.now
     if cost.nil?
       cost = 0
     end
@@ -57,16 +176,16 @@ class SolutionActor
 
     Action.create(
       type: 'SolutionAction',
-      name: 'SetSolutionVolume',
+      name: 'EstimateVolume',
       datetime: datetime,
       metadata: {
         'uuid' => uuid,
-        'volume' => new_volume,
-        'cost' => cost
+        'volume_level_proportion' => volume_level_proportion,
       }
     )
   end
 
+  # TODO: Use the one in QualitativeFitTestingKitActor
   def self.add_to_qlft_kit(uuid:, qlft_kit_uuid:, datetime: nil)
     if datetime.nil?
       datetime = DateTime.now
