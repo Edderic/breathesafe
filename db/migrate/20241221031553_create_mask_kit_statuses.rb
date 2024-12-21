2ca785c39efd473f43fb23d132b11ce139a5a146
Edderic Ugaddan <edderic@gmail.com>
2024-12-20 22:45:02 -0500

    create mask kit status

---
+++ db/migrate/20241221031553_create_mask_kit_statuses.rb
@@ -0,0 +1,13 @@
+class CreateMaskKitStatuses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :mask_kit_statuses do |t|
+      t.uuid :uuid, null: false
+      t.integer :mask_uuid, null: false
+      t.datetime :refresh_datetime, null: false
+
+      t.timestamps
+    end
+
+    add_index :mask_kit_statuses, [:uuid, :mask_uuid, :refresh_datetime], unique: true, name: 'index_uuid_mask_uuid_datetime_on_mkst'
+  end
+end
