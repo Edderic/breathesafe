4d393d59e8de50e10297fa329d8f3d259908b530
Edderic Ugaddan <edderic@gmail.com>
2024-12-20 21:41:58 -0500

    UserStatus.refresh!

--- app/models/user_status.rb
+++ app/models/user_status.rb
@@ -1,2 +1,21 @@
 class UserStatus < ApplicationRecord
+  def self.refresh!(datetime: nil)
+    if datetime.nil?
+      datetime = DateTime.now
+    end
+
+    results = UserStatusBuilder.build
+
+    results.each do |email, details|
+      UserStatus.create(
+        uuid: email,
+        high_risk: details['high_risk'],
+        first_name: details['first_name'],
+        last_name: details['last_name'],
+        address_uuid: details['address_uuid'],
+        refresh_datetime: datetime
+      )
+
+    end
+  end
 end
