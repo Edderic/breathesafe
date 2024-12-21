cc73890fcb248207903ca879ab7b389d3685dcd2
Edderic Ugaddan <edderic@gmail.com>
2024-12-20 21:41:22 -0500

    AddressStatus.refresh!

--- app/models/address_status.rb
+++ app/models/address_status.rb
@@ -1,2 +1,15 @@
 class AddressStatus < ApplicationRecord
+  def self.refresh!(datetime: nil)
+    if datetime.nil?
+      datetime = DateTime.now
+    end
+
+    current_state = AddressStatusBuilder.build
+
+    current_state.each do |uuid, data|
+      data[:uuid] = uuid
+      data[:refresh_datetime] = datetime
+      self.create(**data)
+    end
+  end
 end
