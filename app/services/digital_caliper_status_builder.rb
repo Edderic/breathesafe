3a16c4466d1c3b4e7d211273e6dba1e92792c60a
Edderic Ugaddan <edderic@gmail.com>
2024-12-22 17:48:15 -0500

    rename

--- app/services/digital_caliper_status_builder.rb
+++ app/services/digital_caliper_status_builder.rb
@@ -4,6 +4,11 @@ class DigitalCaliperStatusBuilder
     actions.reduce({}) do |accum, action|
       if action.name == 'CreateDigitalCaliper'
         accum[action.metadata['uuid']] = action.metadata
+      elsif action.name == 'AssociateFacialMeasurementKit'
+        accum[action.metadata['uuid']]['facial_measurement_kit_uuid'] = \
+          action.metadata['facial_measurement_kit_uuid']
+      elsif action.name == 'DissociateFacialMeasurementKit'
+        accum[action.metadata['uuid']]['facial_measurement_kit_uuid'] = nil
       end
 
       accum
