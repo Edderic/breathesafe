5d470b14c93b4360a88e8b778122d8b88b31df9a
Edderic Ugaddan <edderic@gmail.com>
2024-12-22 17:57:59 -0500

    create facial measurement kit status table

---
+++ db/migrate/20241221202323_create_facial_measurement_kit_statuses.rb
@@ -0,0 +1,13 @@
+class CreateFacialMeasurementKitStatuses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :facial_measurement_kit_statuses do |t|
+      t.uuid :uuid, null: false
+      t.datetime :refresh_datetime, null: false
+
+      t.timestamps
+    end
+
+    add_index :facial_measurement_kit_statuses, [:uuid, :refresh_datetime], unique: true,
+      name: 'index_fmks_on_uuid_refr_datetime__unique_true'
+  end
+end
