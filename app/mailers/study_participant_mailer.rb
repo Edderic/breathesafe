531028f507da7309e62548f456c8add92073608b
Edderic Ugaddan <edderic@gmail.com>
2024-12-25 14:48:02 -0500

    study participant mailer

---
+++ app/mailers/study_participant_mailer.rb
@@ -0,0 +1,10 @@
+class StudyParticipantMailer < ApplicationMailer
+  default from: 'info@breathesafe.xyz'
+
+  def shipping_label_assigned
+    @user = params[:user]
+    @shipping_label = params[:shipping_label]
+
+    mail(to: @user['email'], subject: "Mask Recommender: Shipping Label created")
+  end
+end
