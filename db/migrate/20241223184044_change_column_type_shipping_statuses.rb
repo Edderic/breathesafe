e1a22c05456c9471ca025980ffbe0e52af69a5bf
Edderic Ugaddan <edderic@gmail.com>
2024-12-24 21:39:58 -0500

    change column type shipping statuses

---
+++ db/migrate/20241223184044_change_column_type_shipping_statuses.rb
@@ -0,0 +1,6 @@
+class ChangeColumnTypeShippingStatuses < ActiveRecord::Migration[7.0]
+  def change
+    change_column :shipping_statuses, :from_user_uuid, :string
+    change_column :shipping_statuses, :to_user_uuid, :string
+  end
+end
