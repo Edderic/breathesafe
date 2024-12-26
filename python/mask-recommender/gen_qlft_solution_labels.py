aca355cdf1e9153110efb9a60cf444f5aa04b3f5
Edderic Ugaddan <edderic@gmail.com>
2024-12-26 15:14:09 -0500

    more efficient use of 4" x 6" label

---
+++ python/mask-recommender/gen_qlft_solution_labels.py
@@ -0,0 +1,32 @@
+def qlft_solution(aerosol_type='Saccharin', solution_type='Fit Test'):
+    return f"""
+    <tr>
+        <td>
+            <h4 style='padding-right: 1em'>{aerosol_type} {solution_type} Solution</h4>
+        </td>
+        <td>
+            <h4>{aerosol_type} {solution_type} Solution</h4>
+        </td>
+    </tr>
+    """
+
+if __name__ == '__main__':
+    amount = 7
+
+    collection = ""
+    for i in range(amount):
+        collection += qlft_solution(aerosol_type='Saccharin', solution_type='Fit Test')
+        collection += qlft_solution(aerosol_type='Saccharin', solution_type='Sensitivity')
+
+    print(
+    f"""
+    <html>
+        <body>
+            <table>
+                {collection}
+            </table>
+        </body>
+    </html>
+    """
+    )
+
