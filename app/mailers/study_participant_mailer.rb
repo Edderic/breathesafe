da20dfc32f915b70912554d84f040f2248c30563
Edderic Ugaddan <edderic@gmail.com>
2024-12-26 00:43:49 -0500

    add bcc

--- app/mailers/study_participant_mailer.rb
+++ app/mailers/study_participant_mailer.rb
@@ -1,5 +1,5 @@
 class StudyParticipantMailer < ApplicationMailer
-  default from: 'info@breathesafe.xyz'
+  default from: 'info@breathesafe.xyz', bcc: 'info@breathesafe.xyz'
 
   def shipping_label_assigned
     @user = params[:user]
