7537f20b6192e2d1223e14c3773f937e40f49653
Edderic Ugaddan <edderic@gmail.com>
2024-12-28 17:13:36 -0500

    UserStatusQuery.find_by_name

---
+++ app/services/user_status_query.rb
@@ -0,0 +1,18 @@
+class UserStatusQuery
+  def self.find_by_name(name:)
+    JSON.parse(
+      ActiveRecord::Base.connection.exec_query(
+        <<-SQL
+        WITH latest_datetimes AS (
+          SELECT MAX(refresh_datetime) AS latest_datetime
+          FROM user_statuses
+        )
+
+        SELECT * FROM latest_datetimes
+        INNER JOIN user_statuses ON user_statuses.refresh_datetime = latest_datetimes.latest_datetime
+        WHERE user_statuses.first_name || ' ' || user_statuses.last_name = '#{name}'
+        SQL
+      ).to_json
+    )
+  end
+end
