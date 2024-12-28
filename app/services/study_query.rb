381c4daaba4ad7d61c6685282539d251501b3d4b
Edderic Ugaddan <edderic@gmail.com>
2024-12-28 17:13:18 -0500

    StudyQuery.default_study

---
+++ app/services/study_query.rb
@@ -0,0 +1,11 @@
+class StudyQuery
+  def self.default_study
+    JSON.parse(
+      ActiveRecord::Base.connection.exec_query(
+        <<-SQL
+      SELECT * FROM study_statuses WHERE name = 'Mask Recommender Based on Facial Features'
+        SQL
+      ).to_json
+    )
+  end
+end
