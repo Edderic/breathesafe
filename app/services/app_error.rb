468763b854a03d3509c446386675570e8f139321
Edderic Ugaddan <edderic@gmail.com>
2024-12-31 09:08:01 -0500

    pass in message to error

--- app/services/app_error.rb
+++ app/services/app_error.rb
@@ -5,5 +5,9 @@ class UnrecognizedActionError < AppError
 end
 
 class UnsuccessfulScrapeError < AppError
+  def initialize(msg="This is a custom exception", exception_type="custom")
+    @exception_type = exception_type
+    super(msg)
+  end
 end
 
