c7e5a671deb440dd138f2ed9de70a36a5577033d
Edderic Ugaddan <edderic@gmail.com>
2024-12-31 09:23:14 -0500

    improved usps autofill

--- app/services/usps_scraper.rb
+++ app/services/usps_scraper.rb
@@ -10,52 +10,61 @@ if defined?(Rails) && Rails.env.development?
     def initialize
     end
 
+    def try(function, message, max_attempts=3)
+      counter = 0
+      success = false
+      while counter < max_attempts
+        puts("Counter: #{counter}, #{message}")
+        begin
+          function.call()
+          success = true
+          counter = max_attempts
+        rescue Selenium::WebDriver::Error::WebDriverError => e
+          counter += 1
+          sleep 5
+        end
+      end
+
+      if !success
+        raise UnsuccessfulScrapeError.new(message)
+      end
+    end
+
     def bulk_purchase_label
       driver = Selenium::WebDriver.for :chrome
       driver.navigate.to("https://cns.usps.com")
-      driver.find_element(:css => '#username').send_keys(ENV['USPS_USERNAME'])
-      driver.find_element(:css => '#password').send_keys(ENV['USPS_PASSWORD'])
-      driver.find_element(:css => '#btn-submit').click()
-      driver.navigate.to("https://cnsb.usps.com")
-
-      debugger
-
-      # Attempt this at least twice
-      driver.find_element(:css => '#file-upload-btn').click()
-      sleep 3
-      driver.find_element(:css => '#select-file-btn').click()
-      debugger
-      driver.find_element(:css => '#formik-file-upload-continue-btn').click()
-      driver.find_element(:css => '#file-upload-import-btn').click()
-      # Confirm Mapping & Import Labels
-      driver.find_element(:css => '#formik-file-upload-continue-btn').click()
       sleep 5
-      driver.find_element(:css => '#formik-file-upload-continue-btn').click()
-      driver.find_element(:css => "button[type='button']").click()
-      driver.find_element(:xpath => "//[contains(text(), 'View in Label Manager')]").click()
+      try(lambda { driver.find_element(:css => '#username').send_keys(ENV['USPS_USERNAME']) }, 'username')
+      sleep 2
+      try(lambda { driver.find_element(:css => '#password').send_keys(ENV['USPS_PASSWORD']) }, 'password')
+      sleep 1
+      try(lambda { driver.find_element(:css => '#btn-submit').click() }, 'submit')
+      sleep 3
+      try(lambda { driver.navigate.to("https://cnsb.usps.com") }, 'visit cnsb.usps.com')
 
-      rows[0].text.split("\n")
-      rows[0].find_element('td[aria-label="recipient-info"]').text
-      rows[0].methods
-      rows[0].find_elements('td')
-
-
-      # Add all the labels
-      driver.find_element(:css => 'th[aria-label="checkbox-all-labels"]').click()
-      # Proceed to payment
-      #
-      driver.find_element(:css => '#proceed-to-payment-btn').click()
-      driver.find_element(:css => '#top-agreement .checkbox').click()
-      driver.find_element(:css => '#pp-button').click()
-      driver.find_element(:css => '#cns-agreement-close').click()
+      try(lambda { driver.navigate.to("https://cnsb.usps.com/file-upload/define-data") }, 'visit file upload')
 
 
+      try(lambda { driver.find_element(:css => '#select-file-btn').click() }, 'select-file-btn')
+      # Pick a file
+      debugger
 
-      # After payment, save the data to note that purchase labels have been created
-      driver.find_elements(:css => "tbody tr").map{|r| r.text.split("\n")}
-      text_rows = driver.find_elements(:css => "tbody tr").map{|r| r.text.split("\n")}
+      try(lambda { driver.find_element(:css => '#formik-file-upload-continue-btn').click() }, 'file-upload-continue')
+      try(lambda { driver.find_element(:css => '#file-upload-import-btn').click() }, 'file-upload-import-btn')
+      # Confirm Mapping & Import Labels
+      try(lambda { driver.find_element(:css => '#formik-file-upload-continue-btn').click() }, 'file-upload-continue')
+      buttons_text = driver.find_elements(:css => "button[type='button']").map{|x| x.text}
+      buttons_text
+      view_in_label_manager_index = buttons_text.find_index("View In Label Manager")
+      view_in_label_manager_button = driver.find_elements(:css => "button[type='button']")[view_in_label_manager_index]
+      view_in_label_manager_button.click
 
+      driver.find_element(:css => "button#add-all-complete-to-cart-btn").click
+
+
+      # Scrape the stuff to costs
       datetime_now = DateTime.now
+
       datetime_format = [
         datetime_now.year,
         datetime_now.month,
@@ -64,6 +73,66 @@ if defined?(Rails) && Rails.env.development?
         datetime_now.minute,
         datetime_now.second
       ].join('-')
+
+      dict = {}
+      text_rows = driver.find_elements(:css => "tbody tr").map{|r| r.text.split("\n")}
+      len_text_rows = text_rows.count
+
+      text_rows.each.with_index do |row, index|
+        if index == 0 && index >= len_text_rows - 1
+          next
+        end
+
+        dict[row[2]] = {
+          ship_date: row[1],
+          name: row[2],
+          address_line_1: row[3],
+          address_line_2: row[4],
+          memo: row[5],
+          items: row[6],
+          weight: row[9],
+          value: row[10],
+          cost: row[15],
+        }
+      end
+
+      # TODO: add the labels
+      debugger
+
+      # Go to cart
+      driver.navigate.to("https://cnsb.usps.com/cart")
+
+      # Proceed to payment
+      try(lambda {driver.find_element(:css => '#proceed-to-payment-btn').click()}, '#proceed-to-payment-btn')
+      try(lambda {driver.find_element(:css => '#top-agreement .checkbox').click()}, '#top-agreement .checkbox')
+      try(lambda {driver.find_element(:css => '#cc-button').click()}, '#cc-button')
+      try(lambda {driver.find_element(:css => '#cns-agreement-close').click()}, '#cns-agreement-close')
+
+      # Add all the labels
+      # driver.find_element(:css => 'button[data-cy="Label-Actions"]').click()
+
+      text_rows_cart = driver.find_elements(:css => "tbody tr").map{|r| r.text.split("\n")}
+      text_rows_cart = driver.find_elements(:css => "tbody tr").map{|r| r.text.split("\n")}
+      text_rows_cart
+
+      text_rows_cart.each do |cart_row|
+        dict[cart_row[2]]['label_number'] = cart_row[-1]
+      end
+      text_rows_cart
+
+      text_rows_cart.each do |cart_row|
+        dict[cart_row[2]]['label_number'] = cart_row[-1]
+      end
+
+      try(lambda {driver.find_element(:css => '#pp-button').click()}, '#pp-button')
+      try(lambda {driver.find_element(:css => '#cns-agreement-close').click()}, '#cns-agreement-close')
+
+
+
+      # After payment, save the data to note that purchase labels have been created
+      driver.find_elements(:css => "tbody tr").map{|r| r.text.split("\n")}
+      text_rows = driver.find_elements(:css => "tbody tr").map{|r| r.text.split("\n")}
+
       ::CSV.open("#{datetime_format}_usps_labels_created.csv", "wb") do |csv|
         csv << [
           'ship_date',
@@ -77,19 +146,10 @@ if defined?(Rails) && Rails.env.development?
           'value',
           'label_number',
         ]
-        text_rows.each do |r|
-          csv << [
-            r[1],
-            r[2],
-            r[3],
-            r[4],
-            r[5],
-            r[6],
-            r[7],
-            r[8],
-            r[10],
-            r[12]
-          ]
+        dict.each do |key, value|
+          if value.key?('label_number')
+            csv << value.map{|k,v| v}
+          end
         end
       end
 
