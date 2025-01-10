1226eae27d69143ef175f9a4fc088e6fbfa0f774
Edderic Ugaddan <edderic@gmail.com>
2025-01-09 22:25:22 -0500

    progress method for StudyParticipantMailer

--- app/mailers/study_participant_mailer.rb
+++ app/mailers/study_participant_mailer.rb
@@ -7,4 +7,11 @@ class StudyParticipantMailer < ApplicationMailer
 
     mail(to: @user['email'], subject: "Mask Recommender: Shipping Label created")
   end
+
+  def progress
+    @progresses = params[:progresses]
+    @to_email = params[:to_email]
+
+    mail(to: 'info@breathesafe.xyz', subject: "Mask Recommender: Data Collection Progress")
+  end
 end
