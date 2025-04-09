e44f55db67d90c0b647e677a85c3a18e5a2b5b83
Edderic Ugaddan <edderic@gmail.com>
2025-04-08 23:13:43 -0400

    UspspPreAndPostOrderMerger

---
+++ app/services/usps_pre_and_post_order_merger.rb
@@ -0,0 +1,19 @@
+class UspsPreAndPostOrderMerger
+  class << self
+    def call(pre_order:, post_order:)
+      collection = []
+      pre_order.each do |to_create_row|
+        post_order.each do |order|
+          to_create_row_name = "#{to_create_row['Recipient First Name']} #{to_create_row['Recipient Last Name']}"
+          if order["name"] == to_create_row_name
+            # "order" should be in the argument of merge so that label_number
+            # of the "order" overwrites the to_create_row one.
+            collection << to_create_row.merge(order)
+          end
+        end
+      end
+
+      collection
+    end
+  end
+end
