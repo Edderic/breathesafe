0d5f3c1a808798b5258010b13f470f70420ae944
Edderic Ugaddan <edderic@gmail.com>
2025-04-20 20:36:33 -0400

    add anonymized users for community fit testing

---
+++ app/services/community_fit_testing.rb
@@ -0,0 +1,47 @@
+class CommunityFitTesting
+  class << self
+    def generate_anonymized_users_for_email(email:, num_users:)
+      manager = User.find_by_email(email)
+
+      arr = []
+      num_users.times do
+        first_name = SecureRandom.uuid
+        last_name = ""
+        password = SecureRandom.uuid
+
+
+        email = "#{first_name}@fake.com"
+        ActiveRecord::Base.transaction do
+          user = User.new(
+            email: email,
+            password: SecureRandom.uuid
+          )
+
+          user.skip_confirmation!
+          user.save!
+
+          profile = Profile.create!(
+            user_id: user.id,
+            first_name: first_name,
+            last_name: last_name,
+            measurement_system: 'imperial'
+          )
+
+          ManagedUser.create!(
+            manager_id: manager.id,
+            managed_id: user.id
+          )
+        end
+
+        arr << {
+          email: email,
+          first_name: first_name,
+          last_name: last_name,
+          password: password
+        }
+      end
+
+      arr
+    end
+  end
+end
