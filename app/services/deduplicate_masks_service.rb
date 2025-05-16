0b8a2b7aac8933a7605b753b04b0c510f7e989a1
Edderic Ugaddan <edderic@gmail.com>
2025-05-15 22:55:59 -0400

    deduplicate_masks_service

---
+++ app/services/deduplicate_masks_service.rb
@@ -0,0 +1,30 @@
+class DeduplicateMasksService
+  class << self
+    def call(from_mask_id:, to_mask_id:)
+      fit_tests = FitTest.where(mask_id: from_mask_id)
+
+      ActiveRecord::Base.transaction do
+        fit_tests.each do |fit_test|
+          fit_test.update(mask_id: to_mask_id)
+        end
+
+        MaskKitStatus.destroy_all
+        MaskKitStatus.refresh!
+
+        mask_kit_statuses = MaskKitStatus.where(mask_uuid: from_mask_id)
+        mask_kit_statuses.each do |mks|
+          MaskKitActor.update_mask_id(
+            uuid: mks.uuid,
+            from_mask_id: from_mask_id,
+            to_mask_id: to_mask_id
+          )
+        end
+
+        MaskKitStatus.destroy_all
+        MaskKitStatus.refresh!
+
+        Mask.find(from_mask_id).destroy
+      end
+    end
+  end
+end
