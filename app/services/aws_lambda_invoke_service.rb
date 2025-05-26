d5daca16ebaed1b3f6142ebba47a8045bb7da2f5
Edderic Ugaddan <edderic@gmail.com>
2025-05-26 15:49:33 -0400

    aws_lambda_invoke_service

---
+++ app/services/aws_lambda_invoke_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# This service invokes an AWS Lambda function and logs the request, response, and errors.
+#
+
+class AwsLambdaInvokeService
+  DEFAULT_REGION = "us-east-1"
+  MAX_BODY_ROWS = 100
+  class << self
+    def call(function_name:, payload: {}, region: DEFAULT_REGION)
+      aws_lambda = init_aws_lambda_client(region)
+
+      invoke_lambda(aws_lambda, function_name, payload, audit_log)
+    end
+
+    private
+
+    def init_aws_lambda_client(region)
+      Aws::Lambda::Client.new(Aws::Constants::CREDENTIALS.merge(region:))
+    end
+
+    def invoke_lambda(aws_lambda, function_name, payload, audit_log)
+      result = aws_lambda.invoke(function_name: function_name, payload: payload.to_json)
+      parse_lambda_response(result)
+    end
+
+    def parse_lambda_response(result)
+      raw_response = +""
+      result.payload.each { |chunk| raw_response << chunk }
+      Oj.load(raw_response)
+    rescue => e
+      raise "Failed to parse Lambda response: #{e.message}"
+    end
+  end
+end
