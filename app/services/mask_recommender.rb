32046ae2e8b663c5e52ecee7cb1b7ebb30cbdb56
Edderic Ugaddan <edderic@gmail.com>
2025-05-26 22:04:07 -0400

    combine masks with proba fit

--- app/services/mask_recommender.rb
+++ app/services/mask_recommender.rb
@@ -5,6 +5,24 @@ class MaskRecommender
         function_name: 'mask-recommender',
         payload: facial_measurements
       )
+
+      body = Oj.load(response['body'])
+      mask_ids = body['mask_id'].map{|k,v| v}
+      proba_fits = body['proba_fit'].map{|k,v| v}
+      masks = Mask.where('id in (?)', mask_ids)
+
+      collection = []
+      mask_ids.each.with_index do |mask_id, i|
+        proba_fit = proba_fits[i]
+
+        mask = masks.find{|m| m.id == mask_id}
+        collection << {
+          'id' => mask_id,
+          'proba_fit' => proba_fit,
+        }.merge(JSON.parse(mask.to_json))
+      end
+
+      collection.sort{|a,b| b['proba_fit'] <=> a['proba_fit']}
     end
   end
 end
