be923419b9fea3b77bdfdbbc949dc5b6e78ea940
Edderic Ugaddan <edderic@gmail.com>
2025-05-28 22:27:49 -0400

    use with_aggregations

--- app/services/mask_recommender.rb
+++ app/services/mask_recommender.rb
@@ -9,13 +9,13 @@ class MaskRecommender
       body = Oj.load(response['body'])
       mask_ids = body['mask_id'].map{|k,v| v}
       proba_fits = body['proba_fit'].map{|k,v| v}
-      masks = Mask.where('id in (?)', mask_ids)
+      masks = Mask.with_aggregations(mask_ids).to_a
 
       collection = []
       mask_ids.each.with_index do |mask_id, i|
         proba_fit = proba_fits[i]
 
-        mask = masks.find{|m| m.id == mask_id}
+        mask = masks.find{|m| m['id'] == mask_id}
         collection << {
           'id' => mask_id,
           'proba_fit' => proba_fit,
