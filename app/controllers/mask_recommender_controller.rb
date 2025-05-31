53db42f106d7ac7f77cd4d3ea5ab79dd5e0e4f09
Edderic Ugaddan <edderic@gmail.com>
2025-05-30 23:15:38 -0400

    mask recommender controller

---
+++ app/controllers/mask_recommender_controller.rb
@@ -0,0 +1,26 @@
+class MaskRecommenderController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    # TODO: For now, only current user can access facial measurements
+    # Later on, parents should be able to view / edit their children's data
+    status = 200
+    masks = MaskRecommender.call(facial_measurements)
+
+    respond_to do |format|
+      format.json do
+        render json: masks.to_json, status: status
+      end
+    end
+  end
+
+  def facial_measurements
+    params.require(:facial_measurements).permit(
+      :bitragion_subnasale_arc,
+      :face_width,
+      :nose_protrusion,
+      :facial_hair_beard_length_mm
+    )
+  end
+end
+
