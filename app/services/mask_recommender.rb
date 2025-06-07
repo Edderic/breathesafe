384b694b306b38eab6c731ad9dc7dad8bd9adbc8
Edderic Ugaddan <edderic@gmail.com>
2025-06-06 22:43:12 -0400

    don't filter out masks that don't have proba fit score when recommending

--- app/services/mask_recommender.rb
+++ app/services/mask_recommender.rb
@@ -9,20 +9,33 @@ class MaskRecommender
       body = Oj.load(response['body'])
       mask_ids = body['mask_id'].map{|k,v| v}
       proba_fits = body['proba_fit'].map{|k,v| v}
-      masks = Mask.with_aggregations(mask_ids).to_a
+      masks = Mask.with_aggregations.to_a
+      mask_ids_without_recommendation = masks.map{|m| m['id']} - mask_ids
 
       collection = []
-      mask_ids.each.with_index do |mask_id, i|
-        proba_fit = proba_fits[i]
-
+      mask_ids.each.with_index do |mask_id, index|
+        proba_fit = proba_fits[index]
         mask = masks.find{|m| m['id'] == mask_id}
+
         collection << {
           'id' => mask_id,
-          'proba_fit' => proba_fit,
+          'proba_fit' => proba_fit
         }.merge(JSON.parse(mask.to_json))
       end
 
       collection.sort{|a,b| b['proba_fit'] <=> a['proba_fit']}
+
+      mask_ids_without_recommendation.each.with_index do |mask_id, index|
+        proba_fit = nil
+        mask = masks.find{|m| m['id'] == mask_id}
+
+        collection << {
+          'id' => mask_id,
+          'proba_fit' => proba_fit
+        }.merge(JSON.parse(mask.to_json))
+      end
+
+      collection
     end
   end
 end
