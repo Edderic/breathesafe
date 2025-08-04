559f73d6a68bef36eca86e5ed526e6eae9593d4b
Edderic Ugaddan <edderic@gmail.com>
2025-08-03 22:51:04 -0400

    simplify. linting

--- app/controllers/users/facial_measurements_controller.rb
+++ app/controllers/users/facial_measurements_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Users
+  # Controller for facial measurements
   class FacialMeasurementsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
