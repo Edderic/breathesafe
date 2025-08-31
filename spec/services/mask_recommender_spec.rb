6ff4dd5a26132f473624d99f7f432fc78b6a54c0
Edderic Ugaddan <edderic@gmail.com>
2025-08-30 21:58:49 -0400

    fix tests

--- spec/services/mask_recommender_spec.rb
+++ spec/services/mask_recommender_spec.rb
@@ -4,13 +4,16 @@ require 'rails_helper'
 require 'oj'
 
 RSpec.describe MaskRecommender, type: :service do
+  let(:old_env) do
+    ENV['HEROKU_ENVIRONMENT']
+  end
+
   before do
-    @old_env = ENV['HEROKU_ENVIRONMENT']
     ENV['HEROKU_ENVIRONMENT'] = 'staging'
   end
 
   after do
-    ENV['HEROKU_ENVIRONMENT'] = @old_env
+    ENV['HEROKU_ENVIRONMENT'] = old_env
   end
 
   describe '.infer' do
@@ -37,7 +40,7 @@ RSpec.describe MaskRecommender, type: :service do
 
       allow(Mask).to receive(:with_aggregations).and_return(masks)
 
-      expect(AwsLambdaInvokeService).to receive(:call)
+      allow(AwsLambdaInvokeService).to receive(:call)
         .with(
           function_name: 'mask-recommender-staging',
           payload: hash_including(
@@ -48,6 +51,16 @@ RSpec.describe MaskRecommender, type: :service do
         ).and_return({ 'body' => Oj.dump(body) })
 
       result = described_class.infer(facial, mask_ids: [111, 222])
+
+      expect(AwsLambdaInvokeService).to have_received(:call)
+        .with(
+          function_name: 'mask-recommender-staging',
+          payload: hash_including(
+            method: 'infer',
+            facial_measurements: facial,
+            mask_ids: [111, 222]
+          )
+        )
 
       by_id = result.index_by { |h| h['id'] }
       expect(by_id[222]['proba_fit']).to be_within(1e-6).of(0.9)
@@ -65,7 +78,7 @@ RSpec.describe MaskRecommender, type: :service do
         'metrics' => { 'val_acc' => 0.8 }
       }
 
-      expect(AwsLambdaInvokeService).to receive(:call)
+      allow(AwsLambdaInvokeService).to receive(:call)
         .with(
           function_name: 'mask-recommender-staging',
           payload: hash_including(
@@ -77,6 +90,17 @@ RSpec.describe MaskRecommender, type: :service do
         ).and_return({ 'body' => Oj.dump(body) })
 
       result = described_class.train(epochs: 5, data_url: 'https://example.com/data.json', target_col: 'qlft_pass')
+
+      expect(AwsLambdaInvokeService).to have_received(:call)
+        .with(
+          function_name: 'mask-recommender-staging',
+          payload: hash_including(
+            method: 'train',
+            epochs: 5,
+            data_url: 'https://example.com/data.json',
+            target_col: 'qlft_pass'
+          )
+        )
 
       expect(result['message']).to eq(body['message'])
       expect(result.dig('artifacts', 'model_latest')).to eq('s3://bucket/key.pt')
