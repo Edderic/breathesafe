813ab96a3272bd0c537d4421a59d3ac620821fb4
Edderic Ugaddan <edderic@gmail.com>
2025-11-23 20:34:44 -0500

    fix migration

--- db/migrate/20251123182807_add_bulk_fit_tests_import_id_to_masks.rb
+++ db/migrate/20251123182807_add_bulk_fit_tests_import_id_to_masks.rb
@@ -3,6 +3,5 @@
 class AddBulkFitTestsImportIdToMasks < ActiveRecord::Migration[7.0]
   def change
     add_reference :masks, :bulk_fit_tests_import, null: true, foreign_key: true
-    add_index :masks, :bulk_fit_tests_import_id
   end
 end
--- db/migrate/20251123182808_add_bulk_fit_tests_import_id_to_fit_tests.rb
+++ db/migrate/20251123182808_add_bulk_fit_tests_import_id_to_fit_tests.rb
@@ -3,6 +3,5 @@
 class AddBulkFitTestsImportIdToFitTests < ActiveRecord::Migration[7.0]
   def change
     add_reference :fit_tests, :bulk_fit_tests_import, null: true, foreign_key: true
-    add_index :fit_tests, :bulk_fit_tests_import_id
   end
 end
