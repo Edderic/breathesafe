84e1c56b03558476bec8d5ebe1b83b45a7da963d
Edderic Ugaddan <edderic@gmail.com>
2025-12-13 22:44:47 -0500

    bump consent form version

---
+++ config/initializers/consent.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Global, single source of truth for the current consent form version
+# Bump this string when the consent form content changes
+Rails.application.config.x.consent_form_version = '2025-12-13'
