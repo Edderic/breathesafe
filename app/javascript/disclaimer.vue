3f8048996104f169ecf50098ec137b861cb2dfa4
Edderic Ugaddan <edderic@gmail.com>
2025-12-14 12:09:24 -0500

    Modify disclaimer
    Perfect! I've successfully converted the disclaimer markdown to a Vue component. Here's what I implemented:
    
    Changes Made:
    
    1. Same styling as Privacy Policy - Used the grid layout with sidebar TOC and main content area
    2. Normal capitalization - Used title case (e.g., "General Information Disclaimer") instead of ALL CAPS
    3. Mobile-first TOC - Added mobile TOC at the top that shows on small screens
    4. TOC button - Added "TOC" button that appears on mobile and links to #toc-mobile
    5. Functional table of contents - All 10 sections are linked in both desktop and mobile TOCs
    6. Scroll margin - Applied scroll-margin-top: 3.25em to all section anchors to prevent the nav bar from covering content
    7. Unnumbered sections - Kept all sections unnumbered as requested
    8. Proper formatting - Converted markdown formatting to HTML (lists, bold text, paragraphs, horizontal rules)
    
    The disclaimer now has:
    • Desktop sidebar TOC (20% width) with sticky positioning
    • Main content area (80% width) with all disclaimer sections
    • Mobile TOC at the top (hidden on desktop)
    • TOC button (visible only on mobile)
    • Responsive design that switches to single column on mobile
    • All the same visual styling as the privacy policy page
    
    You can now visit http://localhost:3000/#/disclaimer to see the updated disclaimer page with the new content and styling!

--- app/javascript/disclaimer.vue
+++ app/javascript/disclaimer.vue
@@ -1,43 +1,345 @@
 <template>
-<div class='margined'>
-      <div><div align="center"><a name="_4r5vko5di6yg"></a><strong><span>DISCLAIMER</span></strong></div><div align="center"><a name="_l2jmcqu2bv4x"></a></div><div align="center"><br></div><div align="center"><span><strong>Last updated&nbsp;</strong><bdt><strong>April 07, 2023</strong></bdt></span></div><div><br></div><div><br></div><div><a name="_xs0r05tcjblb"></a><strong><span>WEBSITE DISCLAIMER</span></strong></div></div><div><br></div><div><div><span>The information provided by <bdt>Breathesafe LLC</bdt> (<bdt></bdt>"we," "us," or "our"<bdt></bdt>) on <bdt></bdt><bdt><a href="https://www.breathesafe.xyz" target="_blank" data-custom>https://www.breathesafe.xyz</a></bdt> (the <bdt></bdt>"Site"<bdt></bdt>)<bdt></bdt> is for general informational purposes only. All information on <span><bdt></bdt>the Site<bdt></bdt></span> is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability, or completeness of any information on <span><span><bdt></bdt>the Site<bdt></bdt></span></span>. UNDER NO CIRCUMSTANCE SHALL WE HAVE ANY LIABILITY TO YOU FOR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF <span><span><bdt></bdt>THE SITE<bdt></bdt></span></span> OR RELIANCE ON ANY INFORMATION PROVIDED ON <span><span><span><bdt></bdt>THE SITE<bdt></bdt></span></span></span>. YOUR USE OF <span><span><span><bdt></bdt>THE SITE<bdt></bdt></span></span></span> AND YOUR RELIANCE ON ANY INFORMATION ON <span><span><span><bdt></bdt>THE SITE<bdt></bdt></span></span></span> IS SOLELY AT YOUR OWN RISK.</span></div></div><div><br></div><div><div><a name="_x1u8x12nt00e"></a></div><bdt><bdt data-type="conditional-block"><bdt></bdt><bdt data-type="body"><div><strong><span>EXTERNAL LINKS
-DISCLAIMER<br></span></strong></div></bdt></bdt></bdt></div><div><br></div><div><bdt><bdt data-type="conditional-block"><bdt data-type="body"><div><span><span><bdt></bdt>The Site<bdt></bdt></span></span><span><span>&nbsp;may contain (or you may be sent through <span><span><bdt></bdt>the Site<bdt></bdt></span></span>) links</span></span><span>&nbsp;to other
-websites or content belonging to or originating from third parties or links to
-websites and features in banners or other advertising. Such external links are
-not investigated, monitored, or checked for accuracy, adequacy, validity, reliability,
-availability, or completeness by us. WE DO NOT WARRANT, ENDORSE, GUARANTEE, OR
-ASSUME RESPONSIBILITY FOR THE ACCURACY OR RELIABILITY OF ANY INFORMATION
-OFFERED BY THIRD-PARTY WEBSITES LINKED THROUGH THE SITE OR ANY WEBSITE OR
-FEATURE LINKED IN ANY BANNER OR OTHER ADVERTISING. WE WILL NOT BE A PARTY TO OR
-IN ANY WAY BE RESPONSIBLE FOR MONITORING ANY TRANSACTION BETWEEN YOU AND THIRD-PARTY PROVIDERS OF PRODUCTS OR SERVICES.</span></div></bdt></bdt></bdt></div><div><br></div><div><bdt><bdt></bdt></bdt><div><a name="_wfmrqujylbbj"></a></div><bdt><bdt data-type="conditional-block"><bdt></bdt><bdt data-type="body"><div><strong><span>PROFESSIONAL DISCLAIMER</span></strong></div></bdt></bdt></bdt></div><div><br></div><div><bdt><bdt data-type="conditional-block"><bdt data-type="body"><div><span><bdt></bdt>The Site cannot and does not contain <bdt>medical/health</bdt> advice. The <bdt>medical/health</bdt> information is provided for general informational and educational purposes only and is not a substitute for professional advice. Accordingly, before taking any actions based upon such information, we encourage you to consult with the appropriate professionals. We do not provide any kind of <bdt>medical/health</bdt> advice.<bdt></bdt> THE USE OR RELIANCE OF ANY INFORMATION CONTAINED ON <span><span><span><bdt></bdt>THE SITE<bdt></bdt></span></span></span> IS SOLELY AT YOUR OWN RISK.</span></div></bdt></bdt></bdt></div><div><br></div><div><bdt><bdt></bdt></bdt></div><div><bdt></bdt></div><div><bdt data-type="conditional-block"><bdt data-type="body"><div><bdt></bdt></div></bdt></bdt></div><div><bdt data-type="conditional-block"><bdt data-type="body"><div><strong><span>TESTIMONIALS DISCLAIMER</span></strong></div></bdt></bdt><bdt data-type="conditional-block"><bdt data-type="body"><div><br></div></bdt></bdt></div><div><bdt><bdt data-type="conditional-block"><bdt data-type="body"><div><span>The Site may contain testimonials by users of our products and/or services. These testimonials reflect the real-life experiences and opinions of such users. However, the experiences are personal to those particular users, and may not necessarily be representative of all users of our products and/or services. We do not claim, and you should not assume, that all users will have the same experiences. YOUR INDIVIDUAL RESULTS MAY VARY.&nbsp;</span></div><div><br></div><span><bdt><bdt data-type="conditional-block"><bdt></bdt><bdt data-type="body"><div><span>The testimonials on the Site are submitted in various forms such as text, audio and/or video, and are reviewed by us before being posted. They appear on the Site verbatim as given by the users, except for the correction of grammar or typing errors. Some testimonials may have been shortened for the sake of brevity where the full testimonial contained extraneous information not relevant to the general public.<bdt><bdt data-type="conditional-block"><bdt data-type="body"><span><bdt><bdt></bdt></bdt></span></bdt></bdt></bdt></span></div></bdt></bdt></bdt><div><br></div><div><span style="mso-fareast-font-family:Calibri;color:#595959;mso-themecolor:text1;
-mso-themetint:166;">The views and opinions contained in the testimonials belong solely to the individual user and do not reflect our views and opinions. <bdt><bdt data-type="conditional-block"><bdt></bdt><bdt data-type="body"><span style="color: rgb(89, 89, 89); text-align: justify;">We are not affiliated with users who provide testimonials, and users are not paid or otherwise compensated for their testimonials.</span></bdt></bdt><bdt></bdt></bdt></span></div></span></bdt></bdt></bdt></div><div><br></div><div><bdt><bdt data-type="conditional-block"><bdt data-type="body"><span><bdt><bdt data-type="conditional-block"><bdt></bdt><bdt data-type="body"><div><span>The testimonials on the Site are not intended, nor should
-they be construed, as claims that our products and/or services can be used to
-diagnose, treat, mitigate, cure, prevent, or otherwise be used for any disease
-or medical condition. No testimonials have been clinically proven or evaluated.</span></div></bdt></bdt><bdt><bdt><bdt data-type="conditional-block"><bdt data-type="body"><span style="font-size: 15px;"><bdt><bdt></bdt></bdt></span></bdt></bdt></bdt></bdt></bdt></span></bdt></bdt></bdt></div>
-      </div>
+  <router-link to="/disclaimer#toc-mobile">
+    <CircularButton text="TOC" class='toc-button'/>
+  </router-link>
+
+<div class="container">
+  <!-- Mobile TOC - shown at top on mobile -->
+  <div class='toc toc-mobile margined' id='toc-mobile'>
+    <h2><strong>Table of Contents</strong></h2>
+
+    <router-link to="/disclaimer#top">
+      Disclaimer (Breathesafe & SnapFit)
+    </router-link>
+
+    <router-link to="/disclaimer#general-information">
+      General Information Disclaimer
+    </router-link>
+
+    <router-link to="/disclaimer#algorithmic-recommendations">
+      Algorithmic Recommendations Disclaimer
+    </router-link>
+
+    <router-link to="/disclaimer#respirator-use">
+      Respirator Use Disclaimer
+    </router-link>
+
+    <router-link to="/disclaimer#medical-health">
+      Medical / Health Disclaimer
+    </router-link>
+
+    <router-link to="/disclaimer#external-links">
+      External Links Disclaimer
+    </router-link>
+
+    <router-link to="/disclaimer#testimonials">
+      Testimonials Disclaimer
+    </router-link>
+
+    <router-link to="/disclaimer#research-participation">
+      Research Participation Disclaimer
+    </router-link>
+
+    <router-link to="/disclaimer#no-professional-relationship">
+      No Professional Relationship
+    </router-link>
+
+    <router-link to="/disclaimer#contact">
+      Contact
+    </router-link>
+  </div>
+
+<div class="grid">
+
+  <div class='toc toc-desktop margined overflow' id='toc'>
+    <br>
+    <br>
+    <br>
+      <div><br></div><h2><strong>Table of Contents</strong></h2><div><br></div>
+
+      <router-link to="/disclaimer#top">
+        Disclaimer (Breathesafe & SnapFit)
+      </router-link>
+
+      <router-link to="/disclaimer#general-information">
+        General Information Disclaimer
+      </router-link>
+
+      <router-link to="/disclaimer#algorithmic-recommendations">
+        Algorithmic Recommendations Disclaimer
+      </router-link>
+
+      <router-link to="/disclaimer#respirator-use">
+        Respirator Use Disclaimer
+      </router-link>
+
+      <router-link to="/disclaimer#medical-health">
+        Medical / Health Disclaimer
+      </router-link>
+
+      <router-link to="/disclaimer#external-links">
+        External Links Disclaimer
+      </router-link>
+
+      <router-link to="/disclaimer#testimonials">
+        Testimonials Disclaimer
+      </router-link>
+
+      <router-link to="/disclaimer#research-participation">
+        Research Participation Disclaimer
+      </router-link>
+
+      <router-link to="/disclaimer#no-professional-relationship">
+        No Professional Relationship
+      </router-link>
+
+      <router-link to="/disclaimer#contact">
+        Contact
+      </router-link>
+  </div>
+
+  <div class='main margined'>
+    <div id='top'>
+      <h1>Disclaimer (Breathesafe & SnapFit)</h1>
+      <p><strong>Last updated:</strong> December 13, 2025</p>
+      <p>This Disclaimer applies to <strong>Breathesafe LLC</strong> ("Breathesafe," "we," "us," "our") and to all related websites, mobile applications (including <strong>SnapFit</strong>), and services (collectively, the "Services").</p>
+      <p>By using the Services, you acknowledge and agree to the terms of this Disclaimer.</p>
+    </div>
+
+    <hr>
+
+    <div id='general-information'>
+      <br>
+      <br>
+      <h2>General Information Disclaimer</h2>
+      <p>The Services are provided for <strong>general informational and educational purposes only</strong>.</p>
+      <p>All information is provided in good faith; however, we make <strong>no representation or warranty</strong>, express or implied, regarding the accuracy, adequacy, validity, reliability, availability, or completeness of any information provided through the Services.</p>
+      <p><strong>Your use of the Services and reliance on any information provided is solely at your own risk.</strong> To the fullest extent permitted by law, Breathesafe LLC shall not be liable for any loss or damage of any kind arising from your use of, or reliance on, the Services.</p>
+    </div>
+
+    <hr>
+
+    <div id='algorithmic-recommendations'>
+      <br>
+      <br>
+      <h2>Algorithmic Recommendations Disclaimer</h2>
+      <p>SnapFit provides <strong>probabilistic mask-fit recommendations</strong> based on facial measurements and available data.</p>
+      <p>These recommendations:</p>
+      <ul>
+        <li>are <strong>estimates</strong>, not guarantees;</li>
+        <li>do not ensure that a particular mask will achieve an adequate seal; and</li>
+        <li>may be affected by factors outside the scope of the Services, including facial movement, donning technique, strap adjustment, wear over time, and individual variability.</li>
+      </ul>
+      <p>Users are responsible for:</p>
+      <ul>
+        <li>performing appropriate user seal checks, and</li>
+        <li>obtaining qualitative or quantitative fit testing where applicable.</li>
+      </ul>
+    </div>
+
+    <hr>
+
+    <div id='respirator-use'>
+      <br>
+      <br>
+      <h2>Respirator Use Disclaimer</h2>
+      <p>No respirator or face covering provides absolute protection against airborne hazards.</p>
+      <p>The effectiveness of any respirator depends on proper selection, fit, use, and environmental conditions. Use of the Services does <strong>not</strong> guarantee protection against infection, illness, or exposure.</p>
+    </div>
+
+    <hr>
+
+    <div id='medical-health'>
+      <br>
+      <br>
+      <h2>Medical / Health Disclaimer</h2>
+      <p>The Services <strong>do not provide medical advice</strong>, diagnosis, or treatment.</p>
+      <p>Any health-related information provided through the Services is for general informational and educational purposes only and is <strong>not a substitute for professional medical advice</strong>. You should consult qualified healthcare or safety professionals before making decisions related to respiratory protection or health.</p>
+      <p>Reliance on any information provided by the Services is solely at your own risk.</p>
+    </div>
+
+    <hr>
+
+    <div id='external-links'>
+      <br>
+      <br>
+      <h2>External Links Disclaimer</h2>
+      <p>The Services may contain links to third-party websites or content.</p>
+      <p>Breathesafe LLC does <strong>not</strong> investigate, monitor, or verify the accuracy or reliability of third-party content and does not warrant, endorse, or assume responsibility for any third-party websites, products, or services.</p>
+      <p>We are not responsible for any transactions, interactions, or disputes between you and third-party providers.</p>
+    </div>
+
+    <hr>
+
+    <div id='testimonials'>
+      <br>
+      <br>
+      <h2>Testimonials Disclaimer</h2>
+      <p>The Services may include testimonials or user-submitted feedback.</p>
+      <p>Testimonials reflect individual experiences and opinions and <strong>may not be representative of all users</strong>. Individual results may vary.</p>
+      <p>Testimonials:</p>
+      <ul>
+        <li>are not medical claims,</li>
+        <li>are not clinically evaluated, and</li>
+        <li>should not be interpreted as guarantees of outcomes.</li>
+      </ul>
+      <p>Users providing testimonials are not paid or compensated for their statements.</p>
+    </div>
+
+    <hr>
+
+    <div id='research-participation'>
+      <br>
+      <br>
+      <h2>Research Participation Disclaimer</h2>
+      <p>Participation in optional research or data-sharing programs is <strong>voluntary</strong> and <strong>not required</strong> to use baseline SnapFit functionality.</p>
+      <p>Opting in to research participation does not change the non-medical nature of the Services and does not create a healthcare provider–patient relationship.</p>
+    </div>
+
+    <hr>
+
+    <div id='no-professional-relationship'>
+      <br>
+      <br>
+      <h2>No Professional Relationship</h2>
+      <p>Use of the Services does <strong>not</strong> create a medical, healthcare, fiduciary, or professional relationship between you and Breathesafe LLC.</p>
+    </div>
+
+    <hr>
+
+    <div id='contact'>
+      <br>
+      <br>
+      <h2>Contact</h2>
+      <p>Breathesafe LLC<br>
+      138 Miller Ave.<br>
+      Rumford, RI 02916<br>
+      United States<br>
+      Email: <strong>info@breathesafe.xyz</strong></p>
+    </div>
+
+    <br>
+    <br>
+    <br>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
+import CircularButton from './circular_button.vue'
+
 export default {
   name: 'Disclaimer',
   components: {
-  },
-  data() {
-    return {}
-  },
-  props: {
-  },
-  computed: {
-
-  }, methods: {
+    CircularButton
   }
-
 }
 </script>
 
 <style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: 20% 80%;
+    grid-template-rows: auto;
+  }
+
   .margined {
     margin: 1em;
   }
 
+  .main {
+    max-width: 50em;
+  }
+
+  .overflow {
+    overflow-y: auto;
+    height: 100vh;
+    position: sticky;
+    top: 0;
+  }
+
+  .toc {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .toc a {
+    padding: 0.5em;
+    text-decoration: none;
+    color: #333;
+  }
+
+  .toc a:hover {
+    background-color: #f0f0f0;
+  }
+
+  .toc-button {
+    position: fixed;
+    top: 4em;
+    right: 1em;
+    z-index: 1000;
+    display: none;
+  }
+
+  /* Mobile TOC - hidden on desktop */
+  .toc-mobile {
+    display: none;
+  }
+
+  /* Add scroll margin to all anchor sections to account for fixed nav bar */
+  #top,
+  #general-information,
+  #algorithmic-recommendations,
+  #respirator-use,
+  #medical-health,
+  #external-links,
+  #testimonials,
+  #research-participation,
+  #no-professional-relationship,
+  #contact,
+  #toc-mobile {
+    scroll-margin-top: 3.25em;
+  }
+
+  @media(max-width: 700px) {
+    .grid {
+      grid-template-columns: 100%;
+    }
+
+    /* Hide desktop TOC on mobile */
+    .toc-desktop {
+      display: none;
+    }
+
+    /* Show mobile TOC at top */
+    .toc-mobile {
+      display: flex;
+      background-color: #f8f8f8;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      padding: 1em;
+      margin-bottom: 1em;
+    }
+
+    .toc-mobile h2 {
+      font-size: 1.2em;
+      margin: 0 0 0.5em 0;
+    }
+
+    .toc-mobile a {
+      padding: 0.3em 0.5em;
+      font-size: 0.9em;
+    }
+
+    /* Show TOC button on mobile */
+    .toc-button {
+      display: block;
+    }
+  }
 </style>
