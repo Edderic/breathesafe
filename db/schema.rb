74b4a3c62a6853e69610fe277f99cf80bdea13aa
Edderic Ugaddan <edderic@gmail.com>
2025-12-20 22:19:02 -0500

    add deleted_at for soft deletion

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_251_214_191_305) do
+ActiveRecord::Schema[7.0].define(version: 20_251_215_170_000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pg_stat_statements'
   enable_extension 'plpgsql'
@@ -458,7 +458,9 @@ ActiveRecord::Schema[7.0].define(version: 20_251_214_191_305) do
     t.string 'consent_form_version_accepted'
     t.datetime 'consent_form_accepted_at'
     t.jsonb 'forms', default: {}, null: false
+    t.datetime 'deleted_at'
     t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['deleted_at'], name: 'index_users_on_deleted_at'
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
     t.index ['unlock_token'], name: 'index_users_on_unlock_token', unique: true
